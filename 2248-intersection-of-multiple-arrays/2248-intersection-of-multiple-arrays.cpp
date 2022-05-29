adbcd84347113dac38cc8f32bd064b4761b20d7d
Arin Agarwal <arin.agarwal02@gmail.com>
2022-05-29 12:10:56 +0530

    Time: 26 ms (19.22%), Space: 12.6 MB (46.55%) - LeetHub

--- 2248-intersection-of-multiple-arrays/2248-intersection-of-multiple-arrays.cpp
+++ 2248-intersection-of-multiple-arrays/2248-intersection-of-multiple-arrays.cpp
@@ -1,18 +1,19 @@
 class Solution {
 public:
-    vector<int> intersection(vector<vector<int>>& nums){
-        unordered_map<int,int> m;
-        int r=nums.size();
-        for(int i=0;i<r;i++){
+    vector<int> intersection(vector<vector<int>>& nums) {
+        int n = nums.size();
+        map<int,int> mp;
+        for(int i=0;i<n;i++){
             for(int j=0;j<nums[i].size();j++){
-                m[nums[i][j]]++;  // For counting frequency of every element in whole matrix
+                mp[nums[i][j]]++;
             }
         }
         vector<int> ans;
-        for(auto i:m){
-            if(i.second==r) ans.push_back(i.first);  // Pushing only those elements with frequency equal to no. of rows
+        for(auto it:mp){
+            if(it.second==n){
+                ans.push_back(it.first);
+            }
         }
-        sort(ans.begin(),ans.end());  // Sorting the array as answer is desired in the ascending order
         return ans;
     }
 };
