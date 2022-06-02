b9db763d12a8088fd8498c6fe194b7d7eb6ef9a5
Arin Agarwal <arin.agarwal02@gmail.com>
2022-06-02 10:29:00 +0530

    Time: 3 ms (95.42%), Space: 10.1 MB (85.84%) - LeetHub

--- 1822-sign-of-the-product-of-an-array/1822-sign-of-the-product-of-an-array.cpp
+++ 1822-sign-of-the-product-of-an-array/1822-sign-of-the-product-of-an-array.cpp
@@ -1,18 +1,10 @@
 class Solution {
 public:
     int arraySign(vector<int>& nums) {
-        // speedup code with disable sync c and relation cin & cout
-        ios::sync_with_stdio(false); cin.tie(nullptr);
-
-        int neg = 1;
-        for(const int& num : nums){
-            // if num is negative change neg(sign) to reverse
-            if(num < 0)
-                neg *= -1;
-            // if there is a zero value, the product must be zero
-            else if(num == 0)
-                return 0;
-        }
-        return neg;
+        int sum = 1;
+        for (int i = 0; i < nums.size(); ++i)
+            if (nums[i] == 0) return 0;
+            else if (nums[i] < 0) sum *= -1;
+        return sum;
     }
 };
