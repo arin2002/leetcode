a0ce945f0020eb6fe0c86087e22933ccf871636c
Arin Agarwal <arin.agarwal02@gmail.com>
2022-06-04 10:22:56 +0530

    Time: 7 ms (82.63%), Space: 12.1 MB (80.34%) - LeetHub

--- 2164-sort-even-and-odd-indices-independently/2164-sort-even-and-odd-indices-independently.cpp
+++ 2164-sort-even-and-odd-indices-independently/2164-sort-even-and-odd-indices-independently.cpp
@@ -12,8 +12,8 @@ public:
                 v2.push_back(nums[i]);
         }
         sort(v1.begin(),v1.end());
-        sort(v2.begin(),v2.end());
-        reverse(v2.begin(),v2.end());
+        sort(v2.rbegin(),v2.rend());
+        //reverse(v2.begin(),v2.end());
         int j = 0, k =0;
         for(int i = 0 ; i<n;i++)
         {
