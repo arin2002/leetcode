efc186504101c8ebe20b83ab8fff5a2a893032e0
Arin Agarwal <arin.agarwal02@gmail.com>
2022-06-08 16:00:49 +0530

    Time: 28 ms (35.94%), Space: 20.4 MB (22.65%) - LeetHub

--- 605-can-place-flowers/605-can-place-flowers.cpp
+++ 605-can-place-flowers/605-can-place-flowers.cpp
@@ -1,40 +1,49 @@
-class Solution {
-public:
-    bool canPlaceFlowers(vector<int>& nums, int m) {
-        int n = nums.size();
-        
-        if(m==0)
-            return true;
-        
-        if(n==1 && nums[0]==0 && n==1) return true;
-
-        for(int i = 0; i<n; i++)
+class Solution
+{
+    public:
+        bool canPlaceFlowers(vector<int> &nums, int m)
         {
-            if(nums[i] == 0)
+            int n = nums.size();
+
+            if (m == 0)
+                return true;
+
+            if (n == 1 && nums[0] == 0) return true;
+
+            for (int i = 0; i < n; i++)
             {
-                if(i==0){
-                    if(nums[i+1] == 0){
-                    m--;
-                    nums[i] = 1;
+                if (nums[i] == 0)
+                {
+                    if (i == 0)
+                    {
+                        if (nums[i + 1] == 0)
+                        {
+                            m--;
+                            nums[i] = 1;
+                        }
+                    }
+                    else if (i == n - 1)
+                    {
+                        if (nums[i - 1] == 0)
+                        {
+                            m--;
+                            nums[i] = 1;
+                        }
+                    }
+                    else
+                    {
+                        if (nums[i - 1] == 0 && nums[i + 1] == 0)
+                        {
+                            m--;
+                            nums[i] = 1;
+                        }
                     }
                 }
-                else if(i == n-1){
-                    if(nums[i-1] == 0){
-                    m--;
-                    nums[i] =1;
-                    }
-                }
-                else {
-                    if(nums[i-1] == 0 && nums[i+1] == 0){
-                        m--; nums[i] =1 ;
-                    }
-                }   
             }
+            //cout << m;
+            if (m <= 0)
+                return true;
+
+            return false;
         }
-        cout<<m;
-        if(m<=0)
-            return true;
-        
-        return false;
-    }
 };
