3e17eaf5b753b498cd3e42330a020d79db6a869d
Arin Agarwal <arin.agarwal02@gmail.com>
2022-06-10 15:54:25 +0530

    Time: 164 ms (22.50%), Space: 48.5 MB (43.78%) - LeetHub

--- 55-jump-game/55-jump-game.cpp
+++ 55-jump-game/55-jump-game.cpp
@@ -6,16 +6,18 @@ public:
         if(n==1)
             return 1;
         int maxi = 0;
-        for(int i = 0; i<n && maxi>=i; i++)
+        for(int i = 0; i<n-1 && maxi>=i; i++)
         {
-            if(nums[i] == 0) continue;
+            //if(nums[i] == 0) continue;
             maxi = max(nums[i]+i,maxi);
+            if(maxi >= n-1)
+                return true;
             cout<<maxi;
         }
         
-        if(maxi >=n-1)
-            return true;
-        else 
+        // if(maxi >=n-1)
+        //     return true;
+        // else 
             return false;
     }
 };
