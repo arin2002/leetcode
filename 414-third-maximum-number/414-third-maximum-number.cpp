bad880da8801522d41780c9d63dfc5a369bae0f3
Arin Agarwal <arin.agarwal02@gmail.com>
2022-07-09 14:04:58 +0530

    Time: 9 ms (65.20%), Space: 9 MB (79.00%) - LeetHub

---
+++ 414-third-maximum-number/414-third-maximum-number.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int thirdMax(vector<int>& nums) {
+        int n = nums.size();
+        
+        sort(nums.begin(),nums.end());
+        int count = 1, i= n-1;
+        for(; i>=1; i--)
+        {
+            if(count == 3)
+                return nums[i];
+            
+            if(nums[i] != nums[i-1])
+                count++;
+        }
+        
+        if(count == 3)
+            return nums[i];
+        return nums[n-1];
+    }
+};
