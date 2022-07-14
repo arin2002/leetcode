afb42049348972408fb373e2fc1d6232655f305d
Arin Agarwal <arin.agarwal02@gmail.com>
2022-07-14 18:56:25 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (17.00%) - LeetHub

--- 917-reverse-only-letters/917-reverse-only-letters.cpp
+++ 917-reverse-only-letters/917-reverse-only-letters.cpp
@@ -2,26 +2,28 @@ class Solution {
 public:
     string reverseOnlyLetters(string s) {
         int n  = s.length();
-        string letter = "";
         
-        for(int i = 0; i<n; i++)
+        int i = 0;
+        int j = n-1;
+        
+        
+        while(i<j)
         {
-            if(s[i]>='a' && s[i]<='z' || s[i]>='A' && s[i]<='Z'){
-                cout<<1;
-                letter +=s[i];
+            if(s[i]>='A' && s[i]<='Z' || s[i] >= 'a' && s[i] <= 'z')
+            {
+                if(s[j]>='A' && s[j]<='Z' || s[j] >= 'a' && s[j] <= 'z')
+                {
+                    swap(s[i],s[j]);
+                    i++;
+                    j--;
+                }
+                else
+                    j--;
             }
+            else
+                i++;
         }
         
-        reverse(letter.begin(),letter.end());
-        cout<<letter.length();
-        int k = 0;
-        for(int i = 0; i<n; i++)
-        {
-            if(s[i]>='a' && s[i]<='z' || s[i]>='A' && s[i]<='Z'){
-                cout<<1;
-                s[i] = letter[k++];
-            }
-        }
         
         return s;
         
