9d060f4a5c39efd3b8fad4b199ca056b534eb5d4
Arin Agarwal <arin.agarwal02@gmail.com>
2022-07-25 19:34:07 +0530

    Time: 2 ms (82.65%), Space: 5.9 MB (24.83%) - LeetHub

--- 441-arranging-coins/441-arranging-coins.cpp
+++ 441-arranging-coins/441-arranging-coins.cpp
@@ -1,24 +1,27 @@
 class Solution {
 public:
     int arrangeCoins(int n) {
-        int i = 0, j = n;
+//         int i = 0, j = n;
         
-        long long ans = 0;
-        while(i<=j)
-        {
-            long long mid = i + (j-i)/2;
+//         long long ans = 0;
+//         while(i<=j)
+//         {
+//             long long mid = i + (j-i)/2;
             
-            long long temp = (mid*(mid+1))/2;
+//             long long temp = (mid*(mid+1))/2;
             
-            if(temp <=n){
-                ans = max(ans,mid);
-                i = mid+1;
-            }
+//             if(temp <=n){
+//                 ans = max(ans,mid);
+//                 i = mid+1;
+//             }
             
-            else//(temp > n)
-                j = mid - 1;
-        }
+//             else//(temp > n)
+//                 j = mid - 1;
+//         }
         
-        return ans;
+//         return ans;
+        
+        // return sqrt((2*(long)n)+1/4)-1/2;
+        return sqrt(2l*n + .25) - .5;
     }
 };
