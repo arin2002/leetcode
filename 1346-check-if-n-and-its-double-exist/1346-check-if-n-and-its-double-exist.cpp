543d4b4e0f78169e55c6e58bdefc4b6efd6251e1
Arin Agarwal <arin.agarwal02@gmail.com>
2022-07-28 14:58:50 +0530

    Time: 13 ms (41.54%), Space: 11.2 MB (12.99%) - LeetHub

--- 1346-check-if-n-and-its-double-exist/1346-check-if-n-and-its-double-exist.cpp
+++ 1346-check-if-n-and-its-double-exist/1346-check-if-n-and-its-double-exist.cpp
@@ -1,15 +1,24 @@
 class Solution {
 public:
-    bool checkIfExist(vector<int>& arr) {
-        int n = arr.size();
+    bool checkIfExist(vector<int>& nums) {
+        int n = nums.size();
+        
+        unordered_map<int,int> ump;
+        for(auto i : nums)
+            ump[i]++;
         
         for(int i = 0; i<n; i++)
         {
-            for(int j = 0; j<n; j++)
-            {
-                if(j == i)
-                    continue;
-                if(arr[j]*2 == arr[i])
+            long long int temp = nums[i]*2;
+            auto it = ump.find(temp);
+            
+            if(it!=ump.end()){
+                if(nums[i] == 1 || nums[i] == 0)
+                {
+                    if(it->second >=2)
+                        return true;
+                }
+                else
                     return true;
             }
         }
