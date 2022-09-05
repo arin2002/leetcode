c371e88ddf91988e8aadcfff40718a60dee0d2a6
Arin Agarwal <arin.agarwal02@gmail.com>
2022-09-05 14:44:30 +0530

    Time: 10 ms (46.99%), Space: 8.6 MB (20.97%) - LeetHub

--- 338-counting-bits/338-counting-bits.cpp
+++ 338-counting-bits/338-counting-bits.cpp
@@ -1,25 +1,28 @@
 class Solution {
 public:
-
-    
     vector<int> countBits(int n) {
-       vector<int>ans;
-        for(int i=0;i<=n;i++)
-        {
-            int count=0;
-            int k=i;
-            while(k>0)
-            {
-            
-                if((k)&1)
-                {
-                    count++;
-                }
-                k=k>>1;
-            }
+        vector<int> ans;
+        ans.push_back(0);
         
+        if(n==0)
+            return ans;
+        
+        for(int i = 1; i<=n; i++){
+            int count = 0;
+            
+            int k = i;
+            while(k != 0){
+                
+                if(k & 1)
+                    count++;
+                
+                k = k>>1;
+            }
+            
             ans.push_back(count);
         }
+        
         return ans;
+        
     }
 };
