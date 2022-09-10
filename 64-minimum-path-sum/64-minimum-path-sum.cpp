5dbab60a62217d09c657df3efd37cb2c3f18951f
Arin Agarwal <arin.agarwal02@gmail.com>
2022-09-10 21:20:51 +0530

    Time: 10 ms (85.74%), Space: 10.2 MB (31.21%) - LeetHub

--- 64-minimum-path-sum/64-minimum-path-sum.cpp
+++ 64-minimum-path-sum/64-minimum-path-sum.cpp
@@ -1,46 +1,31 @@
 class Solution {
 public:
     
-//     int path(int n, int m,vector<vector<int>>& grid,vector<vector<int>>&dp){
+    
+    int f(int i, int j, vector<vector<int>> &t, vector<vector<int>> &dp){
         
-//         if(n == 0 && m == 0){
-//             return  grid[0][0];
-//         }
-//         if(n<0 || m<0)  return 1000000;
+        if(i<0 || j<0)
+            return 10000;
         
-//         if(dp[n][m] != -1)
-//             return dp[n][m];
+        if(i == 0 && j == 0)
+            return t[i][j];
         
-//         int l = grid[n][m] + path(n-1,m,grid,dp);
-//         int r = grid[n][m] + path(n,m-1,grid,dp);
+        if(dp[i][j] != -1)
+            return dp[i][j];
         
-//         return dp[n][m] = min(l,r);
-//     }
+        int l = t[i][j] + f(i-1,j,t,dp);
+        int r = t[i][j] + f(i,j-1,t,dp);
+        
+        return dp[i][j] = min(l,r);
+    }
+    
     
     int minPathSum(vector<vector<int>>& grid) {
-        int n  = grid.size();
-        int m =  grid[0].size();
+        int m = grid.size();
+        int n = grid[0].size();
         
-        vector<vector<int>> dp(n,vector<int>(m,-1));
-        // return path(n-1,m-1,grid,dp);
+        vector<vector<int>> dp(m,vector<int>(n,-1));
         
-        for(int i = 0; i<n; i++){
-            for(int j = 0; j<m; j++){
-                if(i == 0 && j == 0){
-                    dp[i][j] = grid[0][0];
-                    continue;
-                }
-                int l = INT_MAX, r = INT_MAX;
-                if(i>0)
-                 l = grid[i][j] + dp[i-1][j];
-                
-                if(j>0)
-                 r = grid[i][j] + dp[i][j-1];
-                
-                dp[i][j] = min(l,r);
-            }
-        }
-        
-        return dp[n-1][m-1];
+        return f(m-1,n-1,grid,dp);
     }
 };
