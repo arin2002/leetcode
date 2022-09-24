75f80fd5c4d3c81073a442e64427d01a840d4ebe
Arin Agarwal <arin.agarwal02@gmail.com>
2022-09-24 12:02:52 +0530

    Time: 3 ms (64.67%), Space: 7.2 MB (33.43%) - LeetHub

--- 48-rotate-image/48-rotate-image.cpp
+++ 48-rotate-image/48-rotate-image.cpp
@@ -3,14 +3,15 @@ public:
     void rotate(vector<vector<int>>& nums) {
         int n = nums.size();
         
+        reverse(nums.begin(),nums.end());
         for(int i = 0; i<n; i++){
             for(int j = i+1; j<n; j++){
                 swap(nums[i][j],nums[j][i]);
             }
         }
         
-        for(int i = 0; i<n; i++){
-            reverse(nums[i].begin(),nums[i].end());
-        }
+        // for(int i = 0; i<n; i++){
+        //     reverse(nums[i].begin(),nums[i].end());
+        // }
     }
 };
