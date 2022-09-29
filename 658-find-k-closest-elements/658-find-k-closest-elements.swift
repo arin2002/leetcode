7c372bc88c2ce5fe91700e3a6f5545d102bea259
Arin Agarwal <arin.agarwal02@gmail.com>
2022-09-29 22:36:19 +0530

    Time: 78 ms (70.31%), Space: 31.2 MB (86.01%) - LeetHub

---
+++ 658-find-k-closest-elements/658-find-k-closest-elements.swift
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    vector<int> findClosestElements(vector<int>& arr, int k, int x) {
+        int l,r;
+        r = lower_bound(arr.begin(),arr.end(),x)-arr.begin();
+        l = r-1;
+        while(k--){
+            if(r >= arr.size() || (l>=0 && x-arr[l] <= arr[r]-x))
+                l--;
+            else r++;
+        }
+        return {arr.begin()+l+1,arr.begin()+r};
+    }
+};
