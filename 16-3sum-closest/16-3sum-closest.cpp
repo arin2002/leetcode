466224104376c1ff1a314ca4e660de6df25ffb7c
Arin Agarwal <arin.agarwal02@gmail.com>
2022-10-08 22:09:39 +0530

    Time: 587 ms (28.19%), Space: 16.4 MB (48.38%) - LeetHub

--- 16-3sum-closest/16-3sum-closest.cpp
+++ 16-3sum-closest/16-3sum-closest.cpp
@@ -1,27 +1,29 @@
 class Solution {
 public:
-int threeSumClosest(vector<int>& nums, int target) {
-    if(nums.size() < 3) return 0;
-    int closest = nums[0]+nums[1]+nums[2];
-    sort(nums.begin(), nums.end());
-    for(int first = 0 ; first < nums.size()-2 ; ++first) {
-        if(first > 0 && nums[first] == nums[first-1]) continue;
-        int second = first+1;
-        int third = nums.size()-1;            
-        while(second < third) {
-            int curSum = nums[first]+nums[second]+nums[third];
-            if(curSum == target) return curSum;
-            if(abs(target-curSum)<abs(target-closest)) {
-                closest = curSum;
-            }
-            if(curSum > target) {
-                --third;
-            } else {
-                ++second;
+    int threeSumClosest(vector<int>& nums, int target) {
+        
+        sort(nums.begin(),nums.end());
+        int n = nums.size();
+        int closet = nums[0]+nums[1]+nums[2];
+        
+        for(int i = 0; i<n-2; i++){
+            int l = i+1, r = n-1;
+            
+            while(l<r){
+                int temp = nums[i]+nums[l]+nums[r];
+                
+                if(abs(temp-target) < abs(closet-target))
+                    closet = temp;
+                
+                if(temp<target)
+                    l++;
+                else if(temp>target)
+                    r--;
+                else
+                    break;
             }
         }
+        
+        return closet;
     }
-    return closest;
-}
-
 };
