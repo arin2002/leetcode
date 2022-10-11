29eed9221ab64ab6629577554dcb7690969cc54c
Arin Agarwal <arin.agarwal02@gmail.com>
2022-10-11 16:57:28 +0530

    Time: 213 ms (31.30%), Space: 38.1 MB (80.96%) - LeetHub

--- 2404-most-frequent-even-element/2404-most-frequent-even-element.cpp
+++ 2404-most-frequent-even-element/2404-most-frequent-even-element.cpp
@@ -5,18 +5,25 @@ public:
         int ans = -1, temp = INT_MIN;
         for(auto i : nums){
             if((i&1) == 0){
-                ump[i]++;                
+                ump[i]++;
+                
+             if(ump[i] > temp){
+                temp = ump[i];
+                ans = i;
+            }
+            else if(ump[i] == temp)
+                ans = min(ans,i);   
             }
         }
         
-        for(auto it : ump){
-            if(it.second > temp){
-                temp = it.second;
-                ans = it.first;
-            }
-            else if(it.second == temp)
-                ans = min(ans,it.first);
-        }
+        // for(auto it : ump){
+        //     if(it.second > temp){
+        //         temp = it.second;
+        //         ans = it.first;
+        //     }
+        //     else if(it.second == temp)
+        //         ans = min(ans,it.first);
+        // }
         return ans;
     }
 };
