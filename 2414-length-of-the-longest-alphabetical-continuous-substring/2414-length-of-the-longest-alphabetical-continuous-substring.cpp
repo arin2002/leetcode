01bea0c95a0aced713b6299ea610ba4fdc77dafe
Arin Agarwal <arin.agarwal02@gmail.com>
2022-10-11 14:54:08 +0530

    Time: 47 ms (83.12%), Space: 15.4 MB (46.78%) - LeetHub

---
+++ 2414-length-of-the-longest-alphabetical-continuous-substring/2414-length-of-the-longest-alphabetical-continuous-substring.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int longestContinuousSubstring(string s) {
+        
+        int ans = 1, temp = 1;
+        
+        for(int i = 1; i<s.length(); i++){
+            int curr = s[i]-'a';
+            int prev = s[i-1] - 'a';
+            
+            if(curr-prev == 1){
+                temp++;
+            }
+            else
+                temp = 1;
+            
+            ans = max(ans,temp);
+        }
+        
+        return ans;
+    }
+};
