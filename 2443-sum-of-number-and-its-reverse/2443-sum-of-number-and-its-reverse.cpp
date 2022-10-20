620671aade9572178e322b6ca8ba7b76e705852b
Arin Agarwal <arin.agarwal02@gmail.com>
2022-10-20 16:44:53 +0530

    Time: 64 ms (82.58%), Space: 6 MB (29.30%) - LeetHub

--- 2443-sum-of-number-and-its-reverse/2443-sum-of-number-and-its-reverse.cpp
+++ 2443-sum-of-number-and-its-reverse/2443-sum-of-number-and-its-reverse.cpp
@@ -4,7 +4,7 @@ public:
     int rev(int n){
         int ans = 0;
         while(n>0){
-            ans = ans*10 + n%10;
+            ans = ans * 10 + n%10;
             n/=10;
         }
         
@@ -13,7 +13,7 @@ public:
     
     
     bool sumOfNumberAndReverse(int num) {        
-        for(int i = 0; i<100000; i++){
+        for(int i = 0; i<=num;i++){
             if(i + rev(i) == num){
                 return true;
             }
