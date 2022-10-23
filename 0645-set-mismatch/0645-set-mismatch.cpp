3a29af8846a628b2575a51cfbb3b60170d5aa5be
Arin Agarwal <arin.agarwal02@gmail.com>
2022-10-23 15:26:04 +0530

    Time: 155 ms (18.97%), Space: 30.8 MB (34.35%) - LeetHub

--- 0645-set-mismatch/0645-set-mismatch.cpp
+++ 0645-set-mismatch/0645-set-mismatch.cpp
@@ -2,20 +2,20 @@ class Solution {
 public:
     vector<int> findErrorNums(vector<int>& nums) {
         vector<int> ans(2,0);
-        
-        sort(nums.begin(),nums.end());
+        unordered_map<int,int> ump;
+        // sort(nums.begin(),nums.end());
         int n = nums.size();
         
         int prev = 0;
-        for(int i = 0; i<n; i++){
-            prev += nums[i];
-        }
         
-        for(int i = 1; i<n; i++){
-            if(nums[i] == nums[i-1]){
-                ans[0] = nums[i];
-                break;
-            }
+        for(int  i = 0; i<n; i++){
+            auto it = ump.find(nums[i]);
+            
+            if(it!= ump.end())
+                ans[0] = it->first;
+            
+            prev+=nums[i];
+            ump[nums[i]]++;
         }
         
         int sum = (n*(n+1))/2;
