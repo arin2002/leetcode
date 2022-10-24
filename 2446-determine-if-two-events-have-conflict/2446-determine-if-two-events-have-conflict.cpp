3667219e797c9cc15b77133242060215bfa68caf
Arin Agarwal <arin.agarwal02@gmail.com>
2022-10-24 23:06:07 +0530

    Time: 0 ms (100.00%), Space: 11.1 MB (47.62%) - LeetHub

--- 2446-determine-if-two-events-have-conflict/2446-determine-if-two-events-have-conflict.cpp
+++ 2446-determine-if-two-events-have-conflict/2446-determine-if-two-events-have-conflict.cpp
@@ -1,10 +1,10 @@
-class Solution
-{
+class Solution{
 public:
-    bool haveConflict(vector<string> &e1, vector<string> &e2)
-    {
-        return (e1[0] <= e2[0] && e2[0] <= e1[1]) || 
-               (e2[0] <= e1[0] && e1[0] <= e2[1]);
-
+    bool haveConflict(vector<string> &e1, vector<string> &e2){
+        
+        if(e1[0]<=e2[0] && e2[0] <= e1[1] || e2[0]<=e1[0] && e1[0]<=e2[1])
+            return true;
+        
+        return false;
     }
 };
