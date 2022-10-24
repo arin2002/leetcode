9e9707b337f4dceca5a0e81d4245210464ead70f
Arin Agarwal <arin.agarwal02@gmail.com>
2022-10-24 19:17:09 +0530

    Time: 109 ms (56.76%), Space: 61.2 MB (36.87%) - LeetHub

--- 1239-maximum-length-of-a-concatenated-string-with-unique-characters/1239-maximum-length-of-a-concatenated-string-with-unique-characters.cpp
+++ 1239-maximum-length-of-a-concatenated-string-with-unique-characters/1239-maximum-length-of-a-concatenated-string-with-unique-characters.cpp
@@ -1,44 +1,121 @@
+/* This problem asks us to find the Max Length of Concatenated strings present inside arr[]
+All the concatenated strings should have unique characters
+*/
+
 class Solution {
-public:
-    void fun(int idx,vector<string>& arr,vector<string>& temp,int& maxlength)
+private:
+    
+bool compare(vector<int> &selected, string &currString)
+{
+    // Before checking with selected[], we need to check if currString has repeating characters or Not
+
+    // This is done using selfCheck vector which checks if currString has repeating characters or Not
+    
+    vector<int> selfCheck(26, 0); 
+    
+    for (int i = 0; i < currString.size(); i++)
     {
-        if(idx==arr.size())
+        // As all the characters are present in LowerCase, we can use currString[i] - 'a' to map every character to a corresponding index (as discussed thoroughly in the lecture)
+      
+        // If selfCheck[currString[i] - 'a'] is already taken, it means currString has repeating characters
+        
+        if (selfCheck[currString[i] - 'a'] == 1)
+            return false ;
+
+        // Else we mark all the characters inside SelfCheck as 1
+        
+        selfCheck[currString[i] - 'a'] = 1 ;
+    }
+
+    // The second Loop ensures whether the characters of currString has already been selected or not
+    
+    for (int i = 0; i < currString.size(); i++)
+    {
+        // If currString[i] - 'a' is already taken, it means we cannot take currString, thus we return false
+        
+        if (selected[currString[i] - 'a'] == 1)
+            return false ;
+    }
+
+    
+    // At the end, if currString doesn't contain repeating characters AND it's characters have already not been taken yet, we return true indicating currString can be chosen now
+    
+    return true ;
+}
+
+
+int help(int i, vector<string> &arr, vector<int> &selected, int len)
+{
+    // If we reach till the end of arr[], we need to return the max Length we have taken till now
+    
+    if (i == arr.size() )
+    {
+        return len ;
+    }
+
+    string currString = arr[i] ;
+
+    // If currString contains Duplicate Characters or it's characters have already been taken, we have no option but to skip curr String and move to next Index
+    
+    if (compare(selected, currString) == false)
+    {
+        // skip the currString
+        return help(i + 1, arr, selected, len) ;
+    }
+
+    // Else we have two options, One to include CurrString OR another option is to Skip CurrString
+    
+    else
+    {
+        // If we pick the currString, all it's characters must be marked as taken in selected [] vector
+        
+        for (int j = 0; j < currString.size(); j++)
         {
-            string s1="";
-            for(int i=0;i<temp.size();i++)
-            {
-                s1+=temp[i];
-            }
-            int freq[26]={0};
-            for(int i=0;i<s1.size();i++)
-            {
-                freq[s1[i]-'a']++;
-            }
-            int cnt=0;
-            for(int i=0;i<26;i++)
-            {
-                if(freq[i]==1)
-                {
-                    cnt++;
-                }
-                else if(freq[i]>1)
-                {
-                    cnt=0;
-                    return;
-                }
-            }
-            maxlength=max(maxlength,cnt);
-            return;
+            selected[currString[j] - 'a'] = 1 ;
         }
-        temp.push_back(arr[idx]);
-        fun(idx+1,arr,temp,maxlength);
-        temp.pop_back();
-        fun(idx+1,arr,temp,maxlength);
+
+        // Increase the length by currString.size() 
+        
+        len += currString.size() ;
+
+        // Ask Recursion to do rest of task
+        
+        int op1 = help(i + 1, arr, selected, len) ; 
+
+        // Backtrack and unmark all the characters of currString as NOT TAKEN in selected[] vector
+        
+        for (int j = 0; j < currString.size(); j++)
+        {
+            selected[currString[j] - 'a'] = 0 ;
+        }
+
+        // As we are not including currString, we should decrease len by currString.size()
+        
+        len -= currString.size() ;
+
+        // Ask recursion to do rest of the task
+        
+        int op2 = help(i + 1, arr, selected, len) ;
+
+        // Lastly, we will return the max of (op1 , op2) whichever brings the Longest Length
+        
+        return max(op1, op2) ;
     }
+}
+
+public:
     int maxLength(vector<string>& arr) {
-        vector<string> temp;
-        int maxlength=0;
-        fun(0,arr,temp,maxlength);
-        return maxlength;
-    }
+        
+    // selected [] vector will keep track of all the characters which have been taken yet
+        
+    vector<int> selected(26, 0) ; 
+
+    return help(0, arr, selected, 0);
+        
+}
 };
+
+/*
+    Time Complexity:  O(K * 2^N)
+    Space Complexity: O(N)
+*/
