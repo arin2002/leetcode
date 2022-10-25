55b1d64c12dcf272b08def039a04fe102937c01d
Arin Agarwal <arin.agarwal02@gmail.com>
2022-10-25 11:39:55 +0530

    Time: 4 ms (85.62%), Space: 7.1 MB (50.04%) - LeetHub

--- 0010-regular-expression-matching/0010-regular-expression-matching.cpp
+++ 0010-regular-expression-matching/0010-regular-expression-matching.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    bool f(int i,int j,string &s,string &p)
+    bool f(int i,int j,string &s,string &p, vector<vector<int>> &dp)
     { 
         if(i<0 && j<0) return true;   //Both Strings Exhausted
         if(j<0 && i>=0) return false;  //p exhausted but s is remaining
@@ -14,25 +14,30 @@ public:
             return true;
         }
         
-        if(p[j]==s[i] || p[j]=='.') return f(i-1,j-1,s,p);
+        if(dp[i][j] != -1)
+            return dp[i][j];
+        
+        if(p[j]==s[i] || p[j]=='.') return dp[i][j] = f(i-1,j-1,s,p,dp);
         
         if(p[j]=='*')
         {
             if(p[j-1]!=s[i] && p[j-1]!='.')
-                return f(i,j-2,s,p);  //Consider * to be empty
+                return dp[i][j] = f(i,j-2,s,p,dp);  //Consider * to be empty
             else
             {
-                return f(i,j-2,s,p) || f(i-1,j,s,p); 
+                return dp[i][j] = f(i,j-2,s,p,dp) || f(i-1,j,s,p,dp); 
                 //These three case are as follows
                 //consider * empty
                 //consider * length>=1
             }
         }
-        return false;
+        return dp[i][j] = false;
     }
     
     bool isMatch(string s, string p) {
         int n=s.length(),m=p.length();
-        return f(n-1,m-1,s,p);
+        
+        vector<vector<int>> dp(n,vector<int>(m,-1));
+        return f(n-1,m-1,s,p,dp);
     }
 };
