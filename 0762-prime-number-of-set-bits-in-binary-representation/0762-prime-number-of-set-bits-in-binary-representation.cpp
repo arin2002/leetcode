2536546a539a9974598488aa207d239dc8507f3f
Arin Agarwal <arin.agarwal02@gmail.com>
2022-10-29 13:59:29 +0530

    Time: 3 ms (96.21%), Space: 5.9 MB (78.21%) - LeetHub

--- 0762-prime-number-of-set-bits-in-binary-representation/0762-prime-number-of-set-bits-in-binary-representation.cpp
+++ 0762-prime-number-of-set-bits-in-binary-representation/0762-prime-number-of-set-bits-in-binary-representation.cpp
@@ -1,32 +1,16 @@
 class Solution {
 public:
-    
-    int count(int n){
-        int ans = 0;
-        
-        while(n>0){
-            if(n&1)
-                ans++;
-            
-            n >>= 1;
-        }
-        
-        return ans;
+    bool isprime(int num)
+    {
+        if (num == 2 || num == 3 || num == 5 || num == 7 || num == 11 || num == 13 || num == 17 || num == 19 || num == 23) return 1;
+        return 0;
     }
-    
-    int prime(int x){
-        return (x == 2 || x == 3 || x == 5 || x == 7 ||
-                x == 11 || x == 13 || x == 17 || x == 19);
-    }
-    
     int countPrimeSetBits(int left, int right) {
-        int ans = 0;
-        
-        for(int i = left ; i<=right; i++){
-            int set = count(i);
-            
-            cout<<set;
-            if(prime(set))
+        int ans=0;
+        for(int i=left;i<=right;i++)
+        {
+            int x=__builtin_popcount (i);
+            if(isprime(x) && x!=1)
                 ans++;
         }
         return ans;
