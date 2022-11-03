c5bcc7701b12e938532931ab4c43df242c0ed41e
Arin Agarwal <arin.agarwal02@gmail.com>
2022-11-03 14:12:39 +0530

    Time: 822 ms (49.43%), Space: 167.4 MB (97.54%) - LeetHub

--- 2131-longest-palindrome-by-concatenating-two-letter-words/2131-longest-palindrome-by-concatenating-two-letter-words.cpp
+++ 2131-longest-palindrome-by-concatenating-two-letter-words/2131-longest-palindrome-by-concatenating-two-letter-words.cpp
@@ -1,29 +1,54 @@
 class Solution {
 public:
+    
+    Solution(){
+        ios_base::sync_with_stdio(false);
+        cin.tie(NULL);
+    }
+    
+    string rev (string &s){
+        string t = "";
+        t +=s[1];
+        t +=s[0];
+        
+        return t;
+    }
+    
     int longestPalindrome(vector<string>& words) {
+        int n = words.size();
+        int ans = 0;        
+        unordered_map<string,int> ump;
+        for(auto it : words)
+            ump[it]++;
         
-       int count[26][26] = {}; 
-       int ans =0;
+        int flag = 1;
+        for(auto it : words){
+            string s = rev(it);
+            
+            auto i = ump.find(s);
+            if(it[0] == it[1]){
+                if(i->second > 1){
+                    ans+=4;
+                    ump[it]--;
+                    ump[it]--;
+                }
+                else if(flag && ump[it]>0){
+                    cout<<"hi";
+                    ans+=2;
+                    flag = 0;
+                }
+                else
+                    continue;
+            }
+            else if(i != ump.end() && i->second>0 && ump[it]>0){
+                ans +=4;
+                cout<<i->first<<" ";
+                cout<<ans<<" ";
+                ump[it]--;
+                ump[s]--;
+            }
+        }
         
-			for(auto w : words){
-				int a = w[0] - 'a';
-				int b = w[1] - 'a'; 
-
-				if(count[b][a]){
-					ans+= 4;          
-					count[b][a]--;   // decrement the count as we found mirror word
-				}else
-					count[a][b]++;  //increment the current word count if we not find any mirror word
-			}
-        
-			for(int i=0;i<26;i++){
-				if(count[i][i]){
-					ans+=2;
-					break;
-				}
-			 }
-      
-	return ans;
-         
+        return ans;
     }
 };
