31ac705ee5fccc70da6dcb640d4071324d121dd0
Arin Agarwal <arin.agarwal02@gmail.com>
2022-11-03 14:04:09 +0530

    Time: 423 ms (89.80%), Space: 166.8 MB (100.00%) - LeetHub

--- 2131-longest-palindrome-by-concatenating-two-letter-words/2131-longest-palindrome-by-concatenating-two-letter-words.cpp
+++ 2131-longest-palindrome-by-concatenating-two-letter-words/2131-longest-palindrome-by-concatenating-two-letter-words.cpp
@@ -1,49 +1,21 @@
+#define n 26
+
 class Solution {
 public:
-    string rev (string &s){
-        string t = "";
-        t +=s[1];
-        t +=s[0];
-        
-        return t;
+    Solution() {
+        ios_base::sync_with_stdio(0);
     }
     
     int longestPalindrome(vector<string>& words) {
-        int n = words.size();
-        int ans = 0;        
-        unordered_map<string,int> ump;
-        for(auto it : words)
-            ump[it]++;
-        
-        int flag = 1;
-        for(auto it : words){
-            string s = it;
-            reverse(s.begin(),s.end());
-            
-            auto i = ump.find(s);
-            if(it[0] == it[1]){
-                if(i->second > 1){
-                    ans+=4;
-                    ump[it]--;
-                    ump[it]--;
-                }
-                else if(flag && ump[it]>0){
-                    cout<<"hi";
-                    ans+=2;
-                    flag = 0;
-                }
-                else
-                    continue;
-            }
-            else if(i != ump.end() && i->second>0 && ump[it]>0){
-                ans +=4;
-                cout<<i->first<<" ";
-                cout<<ans<<" ";
-                ump[it]--;
-                ump[s]--;
-            }
+        int len = 0, arr[n][n] = { 0 };
+        for (string& s : words) {
+            if (!arr[s.front() - 'a'][s.back() - 'a']) ++arr[s.back() - 'a'][s.front() - 'a'];
+            else len += 4, --arr[s.front() - 'a'][s.back() - 'a'];
         }
-        
-        return ans;
+        for (int i = 0; i < n; ++i) if (arr[i][i] & 1) {
+            len += 2;
+            break;
+        }
+        return len;
     }
 };
