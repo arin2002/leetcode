a530a77873df5f71925ccea4fe1cd2e104424723
Arin Agarwal <arin.agarwal02@gmail.com>
2022-11-08 14:54:23 +0530

    Time: 71 ms (5.98%), Space: 6.3 MB (82.16%) - LeetHub

--- 1544-make-the-string-great/1544-make-the-string-great.cpp
+++ 1544-make-the-string-great/1544-make-the-string-great.cpp
@@ -1,42 +1,22 @@
 class Solution {
 public:
-    string makeGood(string s) {
-//         for(int i = 0; i<s.length()-1; i++){
-//             if((s[i] >= 'a' && s[i] <= 'z' && s[i+1] >= 'A' && s[i] <= 'Z')){
-                
-//             }
-//         }
-        
-        // int i = 0;
-        // while(i<s.length()-1){
-        //     if(((s[i] >= 'a' && s[i] <= 'z') && (s[i+1] >= 'A' && s[i+1] <= 'Z'))){
-        //         s.erase(i,i+1);
-        //         //cout<<s<<endl;
-        //         i = 0;
-        //     }
-        //     else
-        //         i++;
-        //     cout<<i<<" ";
-        //     cout<<s<<endl;
-        // }
-        
-        
-        stack<char> st;
-        
-        for(auto i :s){
-            if(!st.empty() && abs(i-st.top()) == 32)
-                st.pop();
+    string makeGood(string s) {        
+        int i = 0;
+        while(i<s.length()-1){
+            if(!s.length())
+                break;
+            
+            if(abs(s[i]-s[i+1]) == 32){
+                s.erase(i,2);
+                //cout<<s<<endl;
+                i = 0;
+            }
             else
-                st.push(i);
+                i++;
+            cout<<i<<" ";
+            cout<<s<<endl;
         }
         
-        string ans = "";
-        while(!st.empty()){
-            ans += st.top();
-            st.pop();
-        }
-        
-        reverse(ans.begin(),ans.end());
-        return ans;
+        return s;
     }
 };
