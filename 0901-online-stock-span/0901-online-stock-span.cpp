602a084fe4c1144ec58cfbd1a361390769a390ba
Arin Agarwal <arin.agarwal02@gmail.com>
2022-11-09 10:17:42 +0530

    Time: 1779 ms (5.06%), Space: 86.2 MB (17.70%) - LeetHub

---
+++ 0901-online-stock-span/0901-online-stock-span.cpp
@@ -0,0 +1,27 @@
+class StockSpanner {
+public:
+    
+    vector<int> v;
+    StockSpanner() { 
+    }
+    
+    int next(int price) {
+        v.push_back(price);
+        
+        int ans = 0;
+        for(int i = v.size()-1; i>=0; i--){
+            if(v[i]<=price)
+                ans++;
+            else
+                break;
+        }
+        
+        return ans;
+    }
+};
+
+/**
+ * Your StockSpanner object will be instantiated and called as such:
+ * StockSpanner* obj = new StockSpanner();
+ * int param_1 = obj->next(price);
+ */
