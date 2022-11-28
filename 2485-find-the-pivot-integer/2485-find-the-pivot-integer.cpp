710a827df9a3354ee2d51f41438554295893f1c3
Arin Agarwal <arin.agarwal02@gmail.com>
2022-11-28 19:59:30 +0530

    Time: 0 ms (100.00%), Space: 6 MB (45.45%) - LeetHub

--- 2485-find-the-pivot-integer/2485-find-the-pivot-integer.cpp
+++ 2485-find-the-pivot-integer/2485-find-the-pivot-integer.cpp
@@ -1,35 +1,9 @@
 class Solution {
 public:
-    
-    bool solve(vector<int> &v, int ind){
-        int i = 0, j = ind+1,sum1 = 0, sum2 = 0;
-        
-        for(; i<ind; i++){
-            sum1+=v[i];
-        }
-        
-        for(; j<v.size(); j++)
-            sum2 += v[j];
-        
-        return sum1 == sum2;
-    }
-    
     int pivotInteger(int n) {
-        vector<int> v;
-        
-        if(n == 1)
-            return 1;
-        
-        for(int i = 1; i<=n; i++){
-            v.push_back(i);
-        }
-        
-        for(int i = 1; i<n; i++){
-            if(solve(v,i))
-                return v[i];
-        }
-        
-        
-        return -1;
+        int ans = (n * n + n ) /2;
+        int sq = sqrt(ans);
+        if(sq * sq == ans)return sq;
+        else return -1;
     }
 };
