da97f060666bfe9bd0dba179f4b18b240f504ae2
Arin Agarwal <arin.agarwal02@gmail.com>
2022-11-28 14:01:20 +0530

    Time: 1207 ms (63.71%), Space: 187.9 MB (27.75%) - LeetHub

--- 2225-find-players-with-zero-or-one-losses/2225-find-players-with-zero-or-one-losses.cpp
+++ 2225-find-players-with-zero-or-one-losses/2225-find-players-with-zero-or-one-losses.cpp
@@ -1,31 +1,30 @@
 class Solution {
 public:
     vector<vector<int>> findWinners(vector<vector<int>>& matches) {
-        vector<vector<int>> ans;
+        vector<vector<int>> ans(2,vector<int>());    
         
-        map<int,int> mp1;
-        map<int,int> mp2;
+        vector<int> losser(100001,-1);
         
-        for(int i = 0; i<matches.size(); i++){
-            mp1[matches[i][0]]++;
-            mp2[matches[i][1]]++;
+        for(auto &it : matches){
+            int win = it[0], loss = it[1];
+            
+            if(losser[win] == -1)
+                losser[win] = 0;
+            
+            if(losser[loss] == -1)
+                losser[loss] = 1;
+            
+            else
+                losser[loss]++;
         }
         
-        vector<int> v1,v2;
-        for(auto [i,j] : mp1){
-            if(j >= 1 && mp2.find(i) == mp2.end())
-                v1.push_back(i);
+        for(int i = 1; i<100001; i++){
+            if(losser[i] == 0)
+                ans[0].push_back(i);
+            
+            else if(losser[i] == 1)
+                ans[1].push_back(i);
         }
-        
-        for(auto [i,j] : mp2){
-            if(j == 1)
-                v2.push_back(i);
-        }
-        
-        ans.push_back(v1);
-        ans.push_back(v2);
-        
-        
         return ans;
     }
 };
