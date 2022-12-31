59a981fe795130c354442a08c435967b4262b3d8
Arin Agarwal <arin.agarwal02@gmail.com>
2022-12-31 17:28:42 +0530

    Time: 60 ms (84.10%), Space: 28.3 MB (6.91%) - LeetHub

---
+++ 0561-array-partition/0561-array-partition.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int arrayPairSum(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+        
+        int sum = 0;
+        for(int i = nums.size()-1; i>=1; i-=2){
+            sum += nums[i-1];
+        }
+        
+        return sum;
+    }
+};
