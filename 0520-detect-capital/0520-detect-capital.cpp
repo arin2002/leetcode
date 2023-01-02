b541aa0a642b297139f2a60a14e4de73c2ea7790
Arin Agarwal <arin.agarwal02@gmail.com>
2023-01-02 09:00:00 +0530

    Time: 2 ms (56.84%), Space: 6 MB (89.31%) - LeetHub

--- 0520-detect-capital/0520-detect-capital.cpp
+++ 0520-detect-capital/0520-detect-capital.cpp
@@ -1,35 +1,16 @@
 class Solution {
 public:
     bool detectCapitalUse(string word) {
-        bool flag = false;
-        
-        // lowecase 
-        if(word[0] >=97){
-            for(int  i = 0; i<word.size(); i++){
-                if(word[i]<96)
-                    return false;
+        int cnt=0;
+        int loc;
+        for(int i=0;i<word.length();i++){
+            if(word[i]>='A' && word[i]<='Z'){
+                cnt++;
+                loc = i;
             }
-            
+        }
+        if(cnt == word.length() || cnt == 0 || (cnt==1 && loc==0))
             return true;
-        }
-        
-        // lower
-        if(word[1] >= 97)
-            flag = true;
-        
-        if(flag){
-            for(int i = 1; i<word.size(); i++){
-                if(word[i] < 97)
-                    return false;
-            }
-        }
-        else{
-            for(int i = 1; i<word.size();i++){
-                // cout<<(int)word[i];
-                if(word[i] >= 97)
-                    return false;
-            }
-        }
-        return true;
+        return false;
     }
-};
+};   
