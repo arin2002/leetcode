3885fd6f525064d8d874a6b143fc8df27030395e
Arin Agarwal <arin.agarwal02@gmail.com>
2023-01-06 11:29:29 +0530

    Time: 157 ms (98.79%), Space: 132.6 MB (5.18%) - LeetHub

--- 1833-maximum-ice-cream-bars/1833-maximum-ice-cream-bars.cpp
+++ 1833-maximum-ice-cream-bars/1833-maximum-ice-cream-bars.cpp
@@ -1,16 +1,31 @@
 class Solution {
 public:
-    int maxIceCream(vector<int>& costs, int coins) {        
-        sort(costs.begin(),costs.end());
+    int maxIceCream(vector<int>& costs, long long coins) {        
+        // sort(costs.begin(),costs.end());
+        vector<long long> v(1e5+1,0);
         
-        int ans = 0;
-        for(auto it: costs){
-            if(coins >= it){
-                coins -= it;
-                ans++;
+        for(auto it: costs)
+            v[it]++;
+        
+        long long ans = 0, ind = 1;
+    
+        while(ind <= 1e5){
+            if(v[ind] != 0){
+                if(coins >= (ind*v[ind])){
+                    coins -= (ind*v[ind]);
+                    ans += v[ind];
+                    ind++;
+                }
+                else if (coins >= ind){
+                    coins -= ind;
+                    v[ind]--;
+                    ans++;
+                }
+                else
+                    break;
             }
             else
-                break;
+                ind++;
         }
         
         return ans;
