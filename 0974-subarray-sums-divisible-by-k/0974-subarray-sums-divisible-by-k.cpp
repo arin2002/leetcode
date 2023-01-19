46f439be0de878128a7874259d32040cd9d839a4
Arin Agarwal <arin.agarwal02@gmail.com>
2023-01-19 21:02:42 +0530

    Time: 47 ms (92.06%), Space: 31.6 MB (52.05%) - LeetHub

--- 0974-subarray-sums-divisible-by-k/0974-subarray-sums-divisible-by-k.cpp
+++ 0974-subarray-sums-divisible-by-k/0974-subarray-sums-divisible-by-k.cpp
@@ -1,16 +1,35 @@
 class Solution {
 public:
     int subarraysDivByK(vector<int>& nums, int k) {
-    unordered_map<int, int> mod_count;
-    int count = 0, sum = 0;
-    mod_count[0] = 1;
-    for (int num : nums) {
-        sum += num;
-        int mod = (sum % k + k) % k;
-        count += mod_count[mod];
-        mod_count[mod]++;
+        int n = nums.size();
+        int sum = 0, count = 0;
+        
+        // unordered_map for prefix mod
+        unordered_map<int,int> ump;
+        
+        // for i = 0 case
+        // mtlb  i = 0 pe 5 ha aur divisble 5 ho
+        // par 0 na ho map mai to neglect ho jaygei
+        ump[0] = 1;
+        
+        for(auto it: nums){
+            sum += it;
+            int mod = sum%k;
+            // c,c++ programing languages
+            // give negative mods on -ve numbers
+            // so we have to make this case
+            // maping kar rahe h 
+            // isme
+            if(mod<0)
+                mod += k;
+            auto i = ump.find(mod);
+            if(i != ump.end()){
+                count += (i->second);
+            }
+            
+            ump[mod]++;
+        }
+        
+        return count;
     }
-    return count;
-}
-
 };
