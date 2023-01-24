82facc30358dc9445978d7fcb69dbba6a9617e58
Arin Agarwal <arin.agarwal02@gmail.com>
2023-01-24 12:16:49 +0530

    Time: 8 ms (76.91%), Space: 9.1 MB (61.21%) - LeetHub

--- 0047-permutations-ii/0047-permutations-ii.cpp
+++ 0047-permutations-ii/0047-permutations-ii.cpp
@@ -1,38 +1,20 @@
 class Solution {
 public:
-    
-    vector<int> permu, vis;
-    // set<vector<int>> st;
-    vector<vector<int>> ans;
-    
-    void solve(int ind, vector<int> &nums){
-        if(permu.size() == nums.size()){
-            // st.insert(permu);
-            ans.push_back(permu);
+    void recursion(vector<int> num, int i, int j, vector<vector<int> > &res) {
+        if (i == j-1) {
+            res.push_back(num);
             return;
         }
-        
-        for(int i = 0; i<nums.size(); i++){
-            if(vis[i])
-                continue;
-            
-            if(i>0 && nums[i] == nums[i-1] && !vis[i-1])
-                continue;
-            
-            vis[i] = 1;
-            permu.push_back(nums[i]);
-            solve(ind+1,nums);
-            vis[i] = 0;
-            permu.pop_back();
+        for (int k = i; k < j; k++) {
+            if (i != k && num[i] == num[k]) continue;
+            swap(num[i], num[k]);
+            recursion(num, i+1, j, res);
         }
     }
-    
-    vector<vector<int>> permuteUnique(vector<int>& nums) {
-        vis.resize(30);
-        
-        sort(nums.begin(),nums.end());
-        solve(0,nums);
-        // vector<vector<int>> ans(st.begin(),st.end());
-        return ans;
+    vector<vector<int> > permuteUnique(vector<int> &num) {
+        sort(num.begin(), num.end());
+        vector<vector<int> >res;
+        recursion(num, 0, num.size(), res);
+        return res;
     }
 };
