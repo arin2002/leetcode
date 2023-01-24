50e30fe169069ff725dea9e1bd984f76e57432c3
Arin Agarwal <arin.agarwal02@gmail.com>
2023-01-24 11:00:31 +0530

    Time: 5 ms (87.01%), Space: 9.1 MB (67.00%) - LeetHub

--- 0047-permutations-ii/0047-permutations-ii.cpp
+++ 0047-permutations-ii/0047-permutations-ii.cpp
@@ -2,22 +2,27 @@ class Solution {
 public:
     
     vector<int> permu, vis;
-    set<vector<int>> st;
-    
+    // set<vector<int>> st;
+    vector<vector<int>> ans;
     
     void solve(int ind, vector<int> &nums){
         if(permu.size() == nums.size()){
-            st.insert(permu);
+            // st.insert(permu);
+            ans.push_back(permu);
             return;
         }
         
         for(int i = 0; i<nums.size(); i++){
-            if(vis[i+10])
+            if(vis[i])
                 continue;
-            vis[i+10] = 1;
+            
+            if(i>0 && nums[i] == nums[i-1] && !vis[i-1])
+                continue;
+            
+            vis[i] = 1;
             permu.push_back(nums[i]);
             solve(ind+1,nums);
-            vis[i+10] = 0;
+            vis[i] = 0;
             permu.pop_back();
         }
     }
@@ -25,8 +30,9 @@ public:
     vector<vector<int>> permuteUnique(vector<int>& nums) {
         vis.resize(30);
         
+        sort(nums.begin(),nums.end());
         solve(0,nums);
-        vector<vector<int>> ans(st.begin(),st.end());
+        // vector<vector<int>> ans(st.begin(),st.end());
         return ans;
     }
 };
