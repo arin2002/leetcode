346febb4571856a0f350c373ca719dc7b01843b6
Arin Agarwal <arin.agarwal02@gmail.com>
2023-01-30 17:19:30 +0530

    Time: 0 ms (100.00%), Space: 10.1 MB (8.66%) - LeetHub

--- 0100-same-tree/0100-same-tree.cpp
+++ 0100-same-tree/0100-same-tree.cpp
@@ -11,24 +11,16 @@
  */
 class Solution {
 public:
-    
-    bool solve(TreeNode* p, TreeNode* q){
+    bool isSameTree(TreeNode* p, TreeNode* q) {
         if(!p && !q)
-            return 1;
+            return true;
         
         if(!p || !q)
-            return 0;
-        
+            return false;
         
         if(p->val != q->val)
-            return 0;
+            return false;
         
-        
-        return solve(p->left,q->left) & solve(p->right,q->right);
-    }
-    
-    
-    bool isSameTree(TreeNode* p, TreeNode* q) {
-        return solve(p,q);
+        return isSameTree(p->left,q->left) && isSameTree(p->right,q->right);
     }
 };
