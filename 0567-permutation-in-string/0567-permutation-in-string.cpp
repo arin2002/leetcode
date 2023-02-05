64f20591d13691ae25dc7cf08ee414be8f570402
Arin Agarwal <arin.agarwal02@gmail.com>
2023-02-05 14:42:54 +0530

    Time: 9 ms (67.54%), Space: 7.3 MB (45.40%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.cpp
+++ 0567-permutation-in-string/0567-permutation-in-string.cpp
@@ -1,33 +1,38 @@
 class Solution {
-    bool areVectorsEqual(vector<int> a, vector<int> b){
-        for(int i=0; i<26; i++){
-            if(a[i]!=b[i]) return false;
-        }
-        return true;
-    }
 public:
-    bool checkInclusion(string s1, string s2) {
-        if(s2.size()<s1.size()) return false;
-        vector<int> freqS1(26, 0);
-        for(char c: s1) freqS1[c-'a']++;
-        
-        vector<int> freqS2(26, 0);
-        int i=0, j=0;
-        
-        while(j<s2.size()){
-            freqS2[s2[j]-'a']++;
-            
-            if(j-i+1==s1.size()){
-                if(areVectorsEqual(freqS1, freqS2)) return true;
-            }
-            
-            if(j-i+1<s1.size()) j++;
-            else{
-                freqS2[s2[i]-'a']--;
-                i++;
-                j++;
-            }
+    
+    bool cmp(vector<int> &v){
+        for(auto it : v){
+            if(it != 0)
+                return 0;
         }
-        return false;
+        
+        return 1;
+    }
+    
+    
+    bool checkInclusion(string s1, string s2) {
+        int n = s1.size(), m = s2.size();
+        
+
+        vector<int> v(26,0);
+        for(auto it : s1)
+            v[it-'a']++;
+        
+        int i = 0;
+        for(int j = 0; j<m; j++){
+            v[s2[j]-'a']--;
+            
+            if(j-i+1 < n)
+                continue;
+            
+            if(cmp(v))
+                return 1;
+            
+            v[s2[i]-'a']++;
+            i++;
+        }
+        
+        return 0;
     }
 };
