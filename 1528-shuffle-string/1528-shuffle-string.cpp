3b4894b6b82f7328d57b006ed1dd27d4da1f459a
Arin Agarwal <arin.agarwal02@gmail.com>
2023-02-10 21:12:39 +0530

    Time: 13 ms (40.91%), Space: 15.1 MB (94.79%) - LeetHub

--- 1528-shuffle-string/1528-shuffle-string.cpp
+++ 1528-shuffle-string/1528-shuffle-string.cpp
@@ -1,14 +1,15 @@
 class Solution {
 public:
     string restoreString(string s, vector<int>& indices) {
-        string st(indices.size(),'a');
-        
-        int i = 0;
-        for(auto it : indices){
-            // cout<<it<<i<<endl;
-            st[it] = s[i++];
+        int i=0;
+        while(i!=indices.size()){
+            if(indices[i]==i)i++;
+            else {
+                swap(s[i],s[indices[i]]);
+                swap(indices[i],indices[indices[i]]);
+            }
         }
-        
-        return st;
+        return s;
     }
+
 };
