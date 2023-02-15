902c3ae3f270f6a30b08bb8b76b2e114a842adfb
Arin Agarwal <arin.agarwal02@gmail.com>
2023-02-15 08:38:05 +0530

    Time: 7 ms (96.72%), Space: 10.9 MB (40.34%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -6,17 +6,19 @@ public:
         unordered_map<int,int> ump;
         
         int i = 0, j = nums.size();
-        for(auto it : nums){
-            ump[it] = i++;
-        }
+        // for(auto it : nums){
+        //     ump[it] = i++;
+        // }
         
         for(i = 0; i<j; i++){
             int val = target-nums[i];
             auto it = ump.find(val);
             
-            if(it != ump.end() && i!= it->second){
+            if(it != ump.end()){
                 return {i,it->second};
             }
+            
+            ump[nums[i]] = i;
         }
         
         return ans;
