97d2d5e27ca538907f91e5df8585e42c905feaee
Arin Agarwal <arin.agarwal02@gmail.com>
2023-02-23 13:14:01 +0530

    Time: 18 ms (46.96%), Space: 13.2 MB (74.52%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -1,30 +1,39 @@
 class Solution {
 public:
     void setZeroes(vector<vector<int>>& matrix) {
-        int n = matrix.size(), m = matrix[0].size();
-        
-        vector<pair<int,int>> vp;
-        for(int i = 0; i<n; i++){
-            for(int j = 0; j<m; j++){
-                if(matrix[i][j] == 0)
-                    vp.push_back({i,j});
-            }
+        bool isZeroCol = false;
+        bool isZeroRow = false;
+        for (int i = 0; i < matrix.size(); i++) { //check the first column
+            if (matrix[i][0] == 0) {
+                isZeroCol = true;
+                break;
+            } 
         }
-        
-        if(vp.size() == 0 || vp.size() == n*m)
-            return;
-        
-        for(int i = 0; i<vp.size(); i++){
-            int row = vp[i].first;
-            int col = vp[i].second;
-            
-            for(int j = 0; j<m; j++){
-                matrix[row][j] = 0;
-            }
-            
-            for(int j = 0; j<n; j++){
-                matrix[j][col] = 0;
-            }
+        for (int i = 0; i < matrix[0].size(); i++) { //check the first row
+            if (matrix[0][i] == 0) {
+                isZeroRow = true;
+                break;
+            } 
+        }
+        for (int i = 1; i < matrix.size(); i++) { //check except the first row and column
+            for (int j = 1; j < matrix[0].size(); j++) 
+                if (matrix[i][j] == 0) {
+                    matrix[i][0] = 0;
+                    matrix[0][j] = 0;
+                }
+        }
+        for (int i = 1; i < matrix.size(); i++) { //process except the first row and column
+           for (int j = 1; j < matrix[0].size(); j++) 
+               if (matrix[i][0] == 0 || matrix[0][j] == 0)
+                   matrix[i][j] = 0;
+        }
+        if (isZeroCol) { //handle the first column
+            for (int i = 0; i < matrix.size(); i++) 
+                matrix[i][0] = 0;
+        }
+        if (isZeroRow) { //handle the first row
+            for (int i = 0; i < matrix[0].size(); i++) 
+                matrix[0][i] = 0;
         }
     }
 };
