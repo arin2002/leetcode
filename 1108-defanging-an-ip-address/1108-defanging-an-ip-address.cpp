a311ca67709630a32d5cd2b1e8db7a0474fcd365
Arin Agarwal <arin.agarwal02@gmail.com>
2023-02-28 09:46:20 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (72.75%) - LeetHub

--- 1108-defanging-an-ip-address/1108-defanging-an-ip-address.cpp
+++ 1108-defanging-an-ip-address/1108-defanging-an-ip-address.cpp
@@ -1,13 +1,14 @@
 class Solution {
 public:
     string defangIPaddr(string address) {
-        string ans;
+        string ans = "";
         
-        for(auto it: address){
-            if(it == '.')
-                ans += "[.]";
+        for(auto it : address){
+            if(it == '.'){
+                ans += ("[.]");
+            }
             else
-                ans +=it;
+                ans += it;
         }
         
         return ans;
