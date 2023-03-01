f9625af0281beaa4ba8c6929d9fdaf2e83f0632e
Arin Agarwal <arin.agarwal02@gmail.com>
2023-03-01 11:50:27 +0530

    Time: 0 ms (100.00%), Space: 6.4 MB (30.85%) - LeetHub

---
+++ 0921-minimum-add-to-make-parentheses-valid/0921-minimum-add-to-make-parentheses-valid.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int minAddToMakeValid(string s) {
+        stack<char> st;
+        
+        int count = 0;
+        for(auto it : s){
+            if(it == '('){
+                st.push(it);
+            }
+            else{
+                if(!st.empty()){
+                    st.pop();
+                }
+                else
+                    count++;
+            }
+        }
+        
+        return count+st.size();
+    }
+};
