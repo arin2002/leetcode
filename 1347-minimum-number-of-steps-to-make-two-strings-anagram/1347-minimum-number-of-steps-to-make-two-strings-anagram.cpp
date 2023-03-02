99457d949cf649a90d1d3fac57dfc6640698c70b
Arin Agarwal <arin.agarwal02@gmail.com>
2023-03-02 12:03:12 +0530

    Time: 40 ms (97.05%), Space: 16.6 MB (80.00%) - LeetHub

--- 1347-minimum-number-of-steps-to-make-two-strings-anagram/1347-minimum-number-of-steps-to-make-two-strings-anagram.cpp
+++ 1347-minimum-number-of-steps-to-make-two-strings-anagram/1347-minimum-number-of-steps-to-make-two-strings-anagram.cpp
@@ -1,16 +1,23 @@
 class Solution {
 public:
     int minSteps(string s, string t) {
-        unordered_map<char,int>m;
-        int sum=0;
-        for(auto i:s)
-            m[i]++;
-        for(auto i:t)
-            m[i]--;
-        for(auto i:m){
-            if(i.second<0)
-                sum += i.second;
+        
+        int arr[26] = {0};
+        
+        for(auto it : s)
+            arr[it-'a']++;
+        
+        int count = 0;
+        
+        for(auto it : t){
+            // cout<<it<<" "<<arr[it-'a']<<endl;
+            if(arr[it-'a'] != 0)
+                arr[it-'a']--;
+            
+            else
+                count++;
         }
-        return abs(sum);
+        
+        return count;
     }
 };
