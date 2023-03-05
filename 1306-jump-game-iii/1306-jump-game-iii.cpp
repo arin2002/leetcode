1aabc1fd5fda6a3fd3f9ba1ecfc736a248bcae6f
Arin Agarwal <arin.agarwal02@gmail.com>
2023-03-05 18:04:26 +0530

    Time: 47 ms (74.32%), Space: 32.3 MB (67.81%) - LeetHub

--- 1306-jump-game-iii/1306-jump-game-iii.cpp
+++ 1306-jump-game-iii/1306-jump-game-iii.cpp
@@ -1,34 +1,62 @@
 class Solution {
 public:
-    vector<int> vis;
     
-    bool solve(int ind,vector<int>& arr){
-        if(ind >= arr.size())
-            return false;
+//     bool solve(int ind,vector<int>& arr,vector<int> vis){
+//         if(ind >= arr.size())
+//             return false;
         
-        if(arr[ind] == 0)
-            return true;
+//         if(arr[ind] == 0)
+//             return true;
         
-        bool l = false, r = false;
-        if(!vis[ind]){
-            vis[ind] = 1;
-            l = solve(ind+arr[ind],arr);
-            vis[ind] = 0;
-        }
+//         bool l = false, r = false;
+//         if(!vis[ind]){
+//             vis[ind] = 1;
+//             l = solve(ind+arr[ind],arr,vis);
+//             vis[ind] = 0;
+//         }
         
-        // vis[ind] = 0;
+//         // vis[ind] = 0;
         
-        if(!vis[ind]){
-            vis[ind] = 1;
-            r = solve(ind-arr[ind],arr);
-            vis[ind] = 0;
-        }
+//         if(!vis[ind]){
+//             vis[ind] = 1;
+//             r = solve(ind-arr[ind],arr,vis);
+//             vis[ind] = 0;
+//         }
         
-        return l|r;
-    }
+//         return l|r;
+//     }
     
     bool canReach(vector<int>& arr, int start) {
-        vis.resize(arr.size());
-        return solve(start,arr);
+        vector<int> vis(arr.size());
+        // return solve(start,arr,vis);
+        queue<int> q;
+        q.push(start);
+        
+        while(!q.empty()){
+            int ind = q.front();
+            q.pop();
+            
+            int left = ind - arr[ind], right = arr[ind]+ind;
+            if(left >= 0){
+                if(arr[left] == 0)
+                    return true;
+                
+                else if(!vis[left])
+                    q.push(left); 
+            }
+            
+            if(right<arr.size()){
+                if(arr[right] == 0)
+                    return true;
+                
+                else if(!vis[right])
+                    q.push(right);
+            }
+            
+            // mark it visited
+            vis[ind] = 1;
+        }
+        
+        return false;
     }
 };
