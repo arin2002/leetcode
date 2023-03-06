70beb4e3485dee6082b3278efe3e34f81e0f0c2d
Arin Agarwal <arin.agarwal02@gmail.com>
2023-03-06 12:20:27 +0530

    Time: 11 ms (71.96%), Space: 8.9 MB (77.95%) - LeetHub

--- 1769-minimum-number-of-operations-to-move-all-balls-to-each-box/1769-minimum-number-of-operations-to-move-all-balls-to-each-box.cpp
+++ 1769-minimum-number-of-operations-to-move-all-balls-to-each-box/1769-minimum-number-of-operations-to-move-all-balls-to-each-box.cpp
@@ -1,22 +1,35 @@
 class Solution {
 public:
     
-    int solve(int i, string &s){
+//     int solve(int i, string &s){
         
-        int count = 0;
-        for(int j = 0; j<s.size(); j++){
-            if(s[j] == '1')
-                count += abs(j-i);
-        }
+//         int count = 0;
+//         for(int j = 0; j<s.size(); j++){
+//             if(s[j] == '1')
+//                 count += abs(j-i);
+//         }
         
-        return count;
-    }
+//         return count;
+//     }
     
     vector<int> minOperations(string boxes) {
         vector<int> ans(boxes.size());
         
+        int travel = 0, ones = 0;
         for(int i = 0; i<boxes.size(); i++){
-            ans[i] = solve(i,boxes);
+            travel += ones;
+            ans[i] += travel;
+            if(boxes[i] == '1')
+                ones++;
+        }
+        
+        travel = 0, ones = 0;
+        for(int i = boxes.size()-1; i>=0; i--){
+            travel += ones;
+            ans[i] += travel;
+            
+            if(boxes[i] == '1')
+                ones++;
         }
         
         return ans;
