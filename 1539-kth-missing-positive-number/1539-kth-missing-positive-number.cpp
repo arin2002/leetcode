67ee77569a8b43bd082175b7e14c25f01319501e
Arin Agarwal <arin.agarwal02@gmail.com>
2023-03-06 10:57:55 +0530

    Time: 10 ms (27.76%), Space: 9.6 MB (38.05%) - LeetHub

--- 1539-kth-missing-positive-number/1539-kth-missing-positive-number.cpp
+++ 1539-kth-missing-positive-number/1539-kth-missing-positive-number.cpp
@@ -3,9 +3,11 @@ public:
     int findKthPositive(vector<int>& arr, int k) {
         int i = 0, j = arr.size()-1, mid;
         
-        if((arr[j]-(j+1)) == 0)
-            return arr[j]+k;
+        // sorted h pehle he
+        // if((arr[j]-(j+1)) == 0)
+        //     return arr[j]+k;
         
+        // finding vohi sorted wal
         while(i<=j){
             
             mid = (i+j)/2;
@@ -18,10 +20,10 @@ public:
                 j = mid-1;
         }
         
-        cout<<j<<endl;
-        if(j>= 0)
-            return arr[j]+k-(arr[j]-(j+1));
+        // cout<<j<<endl;
+        // if(j>= 0)
+        //     return arr[j]+k-(arr[j]-(j+1));
         
-        return k;
+        return i+k;
     }
 };
