0100364ce32a30a6d22c85d4b83300b13dd3a7de
Arin Agarwal <arin.agarwal02@gmail.com>
2023-03-07 10:46:44 +0530

    Time: 34 ms (31.55%), Space: 22.3 MB (62.79%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
@@ -1,23 +1,16 @@
 class Solution {
 public:
     int singleNonDuplicate(vector<int>& nums) {
-        // keep high at n-2
-        int low = 0, high = nums.size()-2;
+        int low = 0, high = nums.size()-1;
         
-        // for left half
-        // 1st instance = even
-        // 2nd instanc = odd
-        
-        while(low<=high){
+        while(low<high){
             int mid = (low+high)/2;
             
-            // mid^1 trick used if at even ind mid it will give prev ind like at 4 then 3 aa jayega
-            // if odd then mid+1 ho jayega
             if(nums[mid] == nums[mid^1]){
                 low = mid+1;
             }
             else{
-                high = mid-1;
+                high = mid;
             }
         }
         
