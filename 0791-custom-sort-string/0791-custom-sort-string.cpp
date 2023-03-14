01485f40f4b434d0f28414b04a1feb198a90e789
Arin Agarwal <arin.agarwal02@gmail.com>
2023-03-14 09:15:24 +0530

    Time: 0 ms (100.00%), Space: 6 MB (99.83%) - LeetHub

--- 0791-custom-sort-string/0791-custom-sort-string.cpp
+++ 0791-custom-sort-string/0791-custom-sort-string.cpp
@@ -1,26 +1,10 @@
 class Solution {
 public:
-    string customSortString(string order, string s) {
-        unordered_map<char,int> ump;
-        string ans;
-        for(auto &it : s){
-            ump[it]++;
-        }
+    string customSortString(string s, string t) {
+        sort(t.begin(),t.end(),[&](auto a, auto b){
+            return s.find(a)<s.find(b);
+        });
         
-        for(auto &it : order){
-            if(ump.find(it) != ump.end()){
-                string temp(ump[it],it);
-                ans += temp;
-                ump.erase(it);
-            }
-        }
-        
-        for(auto &it : ump){
-            int n = it.second;
-            string temp(n,it.first);
-            ans += temp;
-        }
-        
-        return ans;
+        return t;
     }
 };
