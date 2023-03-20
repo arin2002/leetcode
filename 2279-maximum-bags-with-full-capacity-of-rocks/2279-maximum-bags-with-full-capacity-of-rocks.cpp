0650ccd126a372cc3fff662d70d0b02f05b736bc
Arin Agarwal <arin.agarwal02@gmail.com>
2023-03-20 19:34:38 +0530

    Time: 229 ms (32.45%), Space: 89.7 MB (14.61%) - LeetHub

--- 2279-maximum-bags-with-full-capacity-of-rocks/2279-maximum-bags-with-full-capacity-of-rocks.cpp
+++ 2279-maximum-bags-with-full-capacity-of-rocks/2279-maximum-bags-with-full-capacity-of-rocks.cpp
@@ -1,30 +1,30 @@
 class Solution {
 public:
     int maximumBags(vector<int>& capacity, vector<int>& rocks, int additionalRocks) {
-        int n = int(capacity.size()), fullBags = 0;
-
+        // vector<pair<int,int>> vp;
+        int n = capacity.size();
         
-        // Sort bags by the remaining capacity.
-        vector<int> remainingCapacity(n);
-
-        for (int i = 0; i < n; ++i)
-            remainingCapacity[i] = capacity[i] - rocks[i];
-        sort(remainingCapacity.begin(), remainingCapacity.end());
-
-        // Iterate over sorted bags and fill them using additional rocks.
-        for (int i = 0; i < n; ++i) {
-            // If we can fill the current one, fill it and move on.
-            // Otherwise, stop the iteration.
-            if (additionalRocks >= remainingCapacity[i]) {
-                additionalRocks -= remainingCapacity[i];
-                fullBags++;
-            }
-            else {
-                break;
-            }
+        priority_queue<int, vector<int>, greater<int>> pq;
+        for(int i = 0; i<n; i++){
+            pq.push(capacity[i]-rocks[i]);
         }
         
-        // Return `fullBags` after the iteration stops.
-        return fullBags;
+        int ans = 0;
+        while(!pq.empty() && additionalRocks > 0){
+            int needed = pq.top();
+            
+            if(needed <= additionalRocks){
+                ans++;
+                additionalRocks -= needed;
+            }
+            else if(needed == 0)
+                ans++;
+            else
+                break;
+            
+            pq.pop();
+        }
+        
+        return ans;
     }
 };
