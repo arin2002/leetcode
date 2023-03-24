83f987e55bba5b42d343826ea94cb287f203188b
Arin Agarwal <arin.agarwal02@gmail.com>
2023-03-24 18:57:27 +0530

    Time: 396 ms (68.87%), Space: 113.8 MB (57.37%) - LeetHub

--- 1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero/1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero.cpp
+++ 1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero/1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero.cpp
@@ -17,7 +17,11 @@ public:
         vector<pair<int,int>>adj[n];
         for(auto it:connections)
         {
+            // if we find 1 in this than that edge need to be reversed
+            // as we were able to traverse forward with it
             adj[it[0]].push_back({it[1],1});
+            // these links are for traversing ahead
+            // virtual links so no impact
             adj[it[1]].push_back({it[0],0});
         }
         vector<int>vis(n,0);
