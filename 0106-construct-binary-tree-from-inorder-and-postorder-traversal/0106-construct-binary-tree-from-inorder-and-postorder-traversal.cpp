dbb81665a8c22d6b810304c285f6aba4e6112d61
Arin Agarwal <arin.agarwal02@gmail.com>
2023-03-28 09:56:21 +0530

    Time: 11 ms (94.39%), Space: 26 MB (89.06%) - LeetHub

--- 0106-construct-binary-tree-from-inorder-and-postorder-traversal/0106-construct-binary-tree-from-inorder-and-postorder-traversal.cpp
+++ 0106-construct-binary-tree-from-inorder-and-postorder-traversal/0106-construct-binary-tree-from-inorder-and-postorder-traversal.cpp
@@ -1,16 +1,32 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
-    TreeNode* buildTree(vector<int>& inorder, vector<int>& postorder) {
-        int n = size(inorder), postIdx = n-1;
-        return build(inorder, postorder, 0, n-1, postIdx);
+    
+    TreeNode* solve(int preSt, int preEnd, int &postInd, vector<int>& inorder, vector<int>& postorder){
+        if(preEnd < preSt)
+            return NULL;
+        
+        TreeNode *node = new TreeNode(postorder[postInd--]);
+        int ind = find(inorder.begin(),inorder.end(),node->val)-inorder.begin();
+        
+        node->right = solve(ind+1,preEnd,postInd,inorder,postorder);
+        node->left = solve(preSt,ind-1,postInd,inorder,postorder);
+        
+        return node;
     }
-
-    TreeNode* build(vector<int>& in, vector<int>& post, int inStart, int inEnd, int& postIdx) {
-        if(inStart > inEnd) return nullptr;
-        TreeNode* root = new TreeNode(post[postIdx--]);
-        int inIdx = find(begin(in), end(in), root -> val) - begin(in);
-        root -> right = build(in, post, inIdx+1, inEnd, postIdx);
-        root -> left  = build(in, post, inStart, inIdx-1, postIdx);
-        return root;
+    
+    TreeNode* buildTree(vector<int>& inorder, vector<int>& postorder) {
+        int n = postorder.size()-1;
+        return solve(0,n,n,inorder,postorder);
     }
 };
