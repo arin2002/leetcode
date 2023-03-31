3a13b07210e24b460bb5201f7db6e2f2ef20b3a7
Arin Agarwal <arin.agarwal02@gmail.com>
2023-03-31 11:24:48 +0530

    Time: 18 ms (95.84%), Space: 17.1 MB (75.93%) - LeetHub

--- 0303-range-sum-query-immutable/0303-range-sum-query-immutable.cpp
+++ 0303-range-sum-query-immutable/0303-range-sum-query-immutable.cpp
@@ -2,15 +2,25 @@ class NumArray {
 public:
     vector<int> pre,suff;
     NumArray(vector<int>& nums) {
-        pre = nums;
+        int n = nums.size();
+        pre.resize(n);
+        suff.resize(n);
+        
+        pre[0] = nums[0];
+        for(int i = 1; i<n; i++)
+            pre[i] = pre[i-1]+nums[i];
+        
+        suff[n-1] = nums[n-1];
+        for(int i = n-2; i>=0; i--){
+            suff[i] = suff[i+1] + nums[i];
+        }
     }
     
     int sumRange(int left, int right) {
-        int sum  = 0;
-        for(int i = left; i<=right; i++)
-            sum += pre[i];
-        
-        return sum;
+        if(left-1 < 0)
+            return pre[right];
+
+        return pre[right]-pre[left-1];
     }
 };
 
