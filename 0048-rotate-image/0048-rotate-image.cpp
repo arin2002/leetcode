1bb0a97d47638ea2cb695ae473f7e81bedade217
Arin Agarwal <arin.agarwal02@gmail.com>
2023-04-03 13:54:24 +0530

    Time: 0 ms (100.00%), Space: 7.2 MB (43.69%) - LeetHub

---
+++ 0048-rotate-image/0048-rotate-image.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    void rotate(vector<vector<int>>& matrix) {
+        int n = matrix.size(), m = matrix[0].size();
+        
+        for(int i = 0; i<n; i++){
+            for(int j = i; j<m; j++){
+                swap(matrix[i][j],matrix[j][i]);
+            }
+        }
+        
+        for(auto &it: matrix){
+            reverse(it.begin(),it.end());
+        }
+    }
+};
