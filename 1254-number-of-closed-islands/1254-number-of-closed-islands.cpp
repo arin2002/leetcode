7aecafe836a84c33c17b7a72fc30bb697f10834c
Arin Agarwal <arin.agarwal02@gmail.com>
2023-04-06 22:24:05 +0530

    Time: 38 ms (6.81%), Space: 14.9 MB (6.26%) - LeetHub

--- 1254-number-of-closed-islands/1254-number-of-closed-islands.cpp
+++ 1254-number-of-closed-islands/1254-number-of-closed-islands.cpp
@@ -1,47 +1,52 @@
 class Solution {
 public:
-    int closedIsland(vector<vector<int>>& grid) {
-        int m = grid.size();
-        int n = grid[0].size();
-        vector<vector<bool>> visit(m, vector<bool>(n));
-        int count = 0;
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (grid[i][j] == 0 && !visit[i][j] && bfs(i, j, m, n, grid, visit)) {
-                    count++;
-                }
-            }
-        }
-        return count;
-    }
-
-    bool bfs(int x, int y, int m, int n, vector<vector<int>>& grid, vector<vector<bool>>& visit) {
-        queue<pair<int, int>> q;
-        q.push({x, y});
-        visit[x][y] = 2;
+    
+    vector<int> drs = {-1,0,1,0,-1};
+    
+    bool bfs(int i, int j, vector<vector<int>>& grid){
+        queue<pair<int,int>> q;
+        q.push({i,j});
+        
+        int n = grid.size(), m = grid[0].size();
         bool isClosed = true;
-
-        vector<int> dirx{0, 1, 0, -1};
-        vector<int> diry{-1, 0, 1, 0};
-
-        while (!q.empty()) {
-            x = q.front().first;   // row nnumber
-            y = q.front().second;  // column number
+        while(!q.empty()){
+            int x = q.front().first;
+            int y = q.front().second;
             q.pop();
-
-            for (int i = 0; i < 4; i++) {
-                int r = x + dirx[i];
-                int c = y + diry[i];
-                if (r < 0 || r >= m || c < 0 || c >= n) {
-                    // (x, y) is a boundary cell.
+            grid[x][y] = 1;
+            
+            for(int k = 0; k<4; k++){
+                int nx = x + drs[k];
+                int ny = y + drs[k+1];
+                
+                // boundary point hua to return false kyuki
+                // 4 side se water nahi h
+                // return false na karna direct kyuki hame sare island chaiye
+                // usse connected
+                if(nx < 0 || nx>= n || ny < 0 || ny>=m){
                     isClosed = false;
-                } else if (grid[r][c] == 0 && !visit[r][c]) {
-                    q.push({r, c});
-                    visit[r][c] = true;
+                }
+                else if(grid[nx][ny] == 0){
+                    q.push({nx,ny});
+                }
+            }
+            
+        }
+        return isClosed;
+    }
+    
+    int closedIsland(vector<vector<int>>& grid) {
+        int n = grid.size(), m = grid[0].size();
+        
+        int ans = 0;
+        for(int i = 0; i<n; i++){
+            for(int j = 0; j<m; j++){
+                if(grid[i][j] == 0 && bfs(i,j,grid)){
+                    ans++;
                 }
             }
         }
-
-        return isClosed;
+        
+        return ans;
     }
 };
