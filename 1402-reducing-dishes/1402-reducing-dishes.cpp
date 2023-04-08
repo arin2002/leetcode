ed8b67185a61c7c093e0b6f3921f35e5b4b8baf3
Arin Agarwal <arin.agarwal02@gmail.com>
2023-04-08 10:28:01 +0530

    Time: 7 ms (58.42%), Space: 8.1 MB (76.12%) - LeetHub

--- 1402-reducing-dishes/1402-reducing-dishes.cpp
+++ 1402-reducing-dishes/1402-reducing-dishes.cpp
@@ -1,31 +1,29 @@
 class Solution {
 public:
     int maxSatisfaction(vector<int>& satisfaction) {
-        sort(satisfaction.begin(), satisfaction.end());
+        sort(satisfaction.begin(),satisfaction.end());
         
-        int n = satisfaction.size(), ans = 0;
-        int suffixSum = 0;
-        
-        int start = n-1, time = 1;
-        
-        // find the point till we get sum >= 0
-        // uske baad he apna aayea max
-        // kyuki uske baad to min ho jayega
+        int n = satisfaction.size();
+        int suffix_sum = 0, start = -1;
         for(int i = n-1; i>=0; i--){
-            suffixSum += satisfaction[i];
+            suffix_sum += satisfaction[i];
             
-            if(suffixSum<0)
+            // cout<<suffix_sum;
+            if(suffix_sum < 0)
                 break;
             
-            start--;
+            start = i;
         }
         
-        start++;
+        if(start == -1)
+            return 0;
         
+        int time = 1, ans = 0;
         for(int i = start; i<n; i++){
             ans += satisfaction[i]*time;
             time++;
         }
+        
         return ans;
     }
 };
