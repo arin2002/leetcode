53c28eb3e044e5c2ccaed4a041df75f635f471d7
Arin Agarwal <arin.agarwal02@gmail.com>
2023-05-06 09:42:57 +0530

    Time: 178 ms (19.20%), Space: 47.8 MB (85.47%) - LeetHub

--- 1498-number-of-subsequences-that-satisfy-the-given-sum-condition/1498-number-of-subsequences-that-satisfy-the-given-sum-condition.cpp
+++ 1498-number-of-subsequences-that-satisfy-the-given-sum-condition/1498-number-of-subsequences-that-satisfy-the-given-sum-condition.cpp
@@ -20,23 +20,41 @@ public:
         return ans;
     }
     
+    // binary exponentitaion
+    // a^b
+    int power(long a, int b){
+        int res = 1;
+        while(b){
+            if(b%2){
+                res = (res*a)%mod;
+                b--;
+            }
+            else{
+                a = (a%mod*a%mod)%mod;
+                b/=2;
+            }
+        }
+        
+        return res;
+    }
+    
     int numSubseq(vector<int>& nums, int target) {
         sort(nums.begin(),nums.end());
         int n = nums.size(), ans = 0;
         
         // precalculate power
         // most imp step
-        int power[n];
-        power[0] = 1;
-        for (int i = 1; i < n; ++i) {
-            power[i] = (power[i - 1] * 2) % mod;
-        }
+        // int power[n];
+        // power[0] = 1;
+        // for (int i = 1; i < n; ++i) {
+        //     power[i] = (power[i - 1] * 2) % mod;
+        // }
         
         //  2 pointer can be used more intiutive
         int i = 0, j = n-1;
         while(i<=j){
             if(nums[i]+nums[j] <= target){
-                ans = ((ans%mod)+power[j-i]%mod)%mod;
+                ans = ((ans%mod)+power(2,j-i)%mod)%mod;
                 i++;
             }
             else
