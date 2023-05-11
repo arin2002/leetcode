bd735461d3cb47855145303d0c0615aa28ea4615
Arin Agarwal <arin.agarwal02@gmail.com>
2023-05-11 15:39:45 +0530

    Time: 32 ms (26.48%), Space: 13.2 MB (14.19%) - LeetHub

--- 1035-uncrossed-lines/1035-uncrossed-lines.cpp
+++ 1035-uncrossed-lines/1035-uncrossed-lines.cpp
@@ -2,28 +2,26 @@ class Solution {
 public:
     vector<vector<int>> dp;
     int n,m;
-    int solve(int i, int prev, vector<int>& nums1, vector<int>& nums2){
-        if(i>=nums1.size() )
+    int solve(int i, int j, vector<int>& nums1, vector<int>& nums2){
+        if(i>= n || j>= m)
             return 0;
         
-        if(dp[i][prev+1] != -1)
-            return dp[i][prev+1];
+        if(dp[i][j] != -1)
+            return dp[i][j];
         
         int l = 0, r = 0;
-        for(int j = prev+1; j<nums2.size(); j++){
-            if(nums1[i] == nums2[j]){
-                l = max(l,1+solve(i+1,j,nums1,nums2));
-            }
+        if(nums1[i] == nums2[j]){
+            l = max(l,1+solve(i+1,j+1,nums1,nums2));
         }
         
-        r = solve(i+1,prev,nums1,nums2);
-        return dp[i][prev+1] = max(l,r);
+        r = max(r,max(solve(i+1,j,nums1,nums2),solve(i,j+1,nums1,nums2)));
+        return dp[i][j] = max(l,r);
     }
     
     int maxUncrossedLines(vector<int>& nums1, vector<int>& nums2) {
         n = nums1.size();
         m = nums2.size();
         dp.resize(n+2,vector<int>(m+2,-1));
-        return solve(0,-1,nums1,nums2);
+        return solve(0,0,nums1,nums2);
     }
 };
