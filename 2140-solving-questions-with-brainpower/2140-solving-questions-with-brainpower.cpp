00894dbc4492a3e1c011ccb4c009140a38f523c1
Arin Agarwal <arin.agarwal02@gmail.com>
2023-05-12 10:21:45 +0530

    Time: 404 ms (20.22%), Space: 121.4 MB (53.71%) - LeetHub

--- 2140-solving-questions-with-brainpower/2140-solving-questions-with-brainpower.cpp
+++ 2140-solving-questions-with-brainpower/2140-solving-questions-with-brainpower.cpp
@@ -11,8 +11,8 @@ public:
         if(dp[i] != -1)
             return dp[i];
         
-        long long take = (long)nums[i][0] + 0LL + 1LL*solve(i+nums[i][1]+1,nums);
-        long long notTake = 0 + 1LL*solve(i+1,nums);
+        long long take = nums[i][0] + solve(i+nums[i][1]+1,nums);
+        long long notTake = 0 + solve(i+1,nums);
         
         return dp[i] = max(take,notTake);
     }
