505efe9825d7d96df66151deb43433ba467989c8
Arin Agarwal <arin.agarwal02@gmail.com>
2023-05-16 11:34:56 +0530

    Time: 23 ms (26.37%), Space: 16.5 MB (35.50%) - LeetHub

--- 2466-count-ways-to-build-good-strings/2466-count-ways-to-build-good-strings.cpp
+++ 2466-count-ways-to-build-good-strings/2466-count-ways-to-build-good-strings.cpp
@@ -1,27 +1,34 @@
 class Solution {
 public:
-int Zero=0,One=0;
-int mod = 1000000007;
-    //this is just like climbing stairs
-    int solve(int n,vector<int> &dp){
-        if(n<0)
+    int mod = 1e9+7;
+    int Zero, One;
+    vector<long> dp;
+    
+    long solve(long n){
+        if(n < 0)
             return 0;
-        if(n==0)
+        
+        if(n == 0)
             return 1;
+        
         if(dp[n] != -1)
             return dp[n];
-        return dp[n] = (solve(n-Zero,dp)%mod+solve(n-One,dp)%mod)%mod;
+        
+        int l = solve(n-Zero)%mod;
+        int r = solve(n-One)%mod;
+        
+        return dp[n] = (l%mod+r%mod)%mod;
     }
+    
     int countGoodStrings(int low, int high, int zero, int one) {
-        Zero = zero;
-        One = one;
-        int ans = 0;
-        vector<int> dp(high+1,-1);
-        for(int i=low; i<=high; i++)
-        {
-            int sum = solve(i,dp)%mod;
-            ans = (ans+sum)%mod;
+        Zero = zero; One = one;
+        
+        long ans = 0;
+        dp.resize(high+1,-1);
+        for(int i = low; i<=high; i++){
+            ans = (ans%mod + solve(i)%mod)%mod;
         }
+        
         return ans;
     }
 };
