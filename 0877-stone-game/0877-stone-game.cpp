666b92be8a34463eb35fee67b751224add350951
Arin Agarwal <arin.agarwal02@gmail.com>
2023-05-26 13:45:24 +0530

    Time: 51 ms (9.94%), Space: 16.5 MB (21.82%) - LeetHub

--- 0877-stone-game/0877-stone-game.cpp
+++ 0877-stone-game/0877-stone-game.cpp
@@ -1,6 +1,24 @@
 class Solution {
 public:
+    
+    vector<vector<int>> dp;
+    
+    int solve(int i, int j, vector<int>& nums){
+        if(i == j)
+            return 0;
+        
+        if(dp[i][j] != -1)
+            return dp[i][j];
+        
+        int l = nums[i] - solve(i+1,j,nums);
+        int r = nums[j] - solve(i,j-1,nums);
+        
+        return dp[i][j] = max(l,r);
+    }
+    
     bool stoneGame(vector<int>& piles) {
-        return true;
+        int n = piles.size();
+        dp.resize(n+1,vector<int>(n+1,-1));
+        return solve(0,n-1,piles) > 0;
     }
 };
