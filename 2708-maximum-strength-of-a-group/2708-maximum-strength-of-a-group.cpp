7670c05869b5255ed172821be4256e7ce369935f
Arin Agarwal <arin.agarwal02@gmail.com>
2023-05-28 09:17:06 +0530

    Time: 15 ms (100.00%), Space: 46.6 MB (33.33%) - LeetHub

--- 2708-maximum-strength-of-a-group/2708-maximum-strength-of-a-group.cpp
+++ 2708-maximum-strength-of-a-group/2708-maximum-strength-of-a-group.cpp
@@ -2,19 +2,39 @@ class Solution {
 public:
     long long ans = -1e12;
     
-    void solve(int ind, long long prod, int size,vector<int> &nums){
-        if(ind >= nums.size()){
-            if(size)
-                ans = max(ans,prod);
-            return;
+    long long maxStrength(vector<int>& nums) {
+        long long zero = 0, posCount = 0, negCount = 0, largestN = -1021;
+        long long n = nums.size(), prod = 1;
+        
+        if(n == 1)
+            return nums[0];
+        
+        for(int i = 0; i<n; i++){
+            if(nums[i])
+                prod *= (long long)nums[i];
+            
+            if(nums[i]>0)
+                posCount++;
+            else if(nums[i]<0){
+                negCount++;
+                largestN = max((long)largestN,(long)nums[i]);
+            }
+            else
+                zero = 1;
         }
         
-        solve(ind+1,prod*nums[ind],size+1,nums);
-        solve(ind+1,prod,size,nums);
-    }
-    
-    long long maxStrength(vector<int>& nums) {
-        solve(0,1,0,nums);
-        return ans;
+        if(posCount == 0 && negCount == 0)
+            return 0;
+        
+        if(zero == 1 && negCount == 1 && posCount == 0)
+            return 0;
+        
+        if(negCount%2 == 0)
+            return prod;
+        
+        if(negCount == 0)
+            return prod;
+        
+        return prod/largestN;
     }
 };
