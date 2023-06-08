c41847b4b9806e279ef16d944eb106ae1db0292e
Arin Agarwal <arin.agarwal02@gmail.com>
2023-06-08 12:06:58 +0530

    Time: 115 ms (83.76%), Space: 17.8 MB (41.93%) - LeetHub

--- 2101-detonate-the-maximum-bombs/2101-detonate-the-maximum-bombs.cpp
+++ 2101-detonate-the-maximum-bombs/2101-detonate-the-maximum-bombs.cpp
@@ -1,52 +1,50 @@
 class Solution {
-    #define ll long long int
 public:
-void dfs(int node,vector<int>adj[],vector<int>&vis,int &c)
-{
-    c++;
-    vis[node]=1;
-    for(auto it:adj[node])
-    if(!vis[it])
-    dfs(it,adj,vis,c);
-}
+    
+    void dfs(int node, int &ans, vector<int> &vis, vector<int> adj[]){
+        vis[node] = 1;
+        
+        ans++;
+        for(auto &it: adj[node]){
+            if(!vis[it]){
+                dfs(it,ans,vis,adj);
+            }
+        }
+    }
+    
     int maximumDetonation(vector<vector<int>>& bombs) {
-        int n=bombs.size();
-        vector<int>adj[n+1];
-        for(int i=0;i<n;i++)
-        {
-            ll x1,y1,r1;
-            x1=bombs[i][0];
-            y1=bombs[i][1];
-            r1=bombs[i][2];
-            for(int j=0;j<n;j++)
-            {
-                if(i!=j)
-                {
-                     ll x2,y2,r2;
-                     x2=abs(x1-bombs[j][0]);
-                     y2=abs(y1-bombs[j][1]);
-                    if(x2*x2+y2*y2<=r1*r1)
-                    {
-                        adj[i].push_back(j);
-                    }
+        int n = bombs.size();
+        vector<int> adj[n];
+        
+        for(int i = 0; i<n; i++){
+            long long x1 = bombs[i][0], x2 = bombs[i][1];
+            long long r = bombs[i][2];
+            
+            for(int j = 0; j<n; j++){
+                
+                if(j==i)
+                    continue;
+                
+                long long x3 = bombs[j][0], x4 = bombs[j][1];
+                
+                // try to minimize sqrt divwion opeartion 
+                long long d = (x3-x1)*(x3-x1)+(x4-x2)*(x4-x2);
+                
+                // cout<<d<<" "<<r<<endl;
+                if(r*r >= d){
+                    adj[i].push_back(j);
                 }
             }
         }
-            int maxi=0;
-           
-             for(int i=0;i<n;i++)
-             { 
-                 int c=0;
-                  vector<int>vis(n,0);
-                 dfs(i,adj,vis,c);
-                 if(c>maxi)
-                 {
-                     maxi=c;
-                     
-                 }
-                   
-             }
-             
-             return maxi;
+        
+        int ans = 0;
+        for(int i = 0; i<n; i++){
+            vector<int> vis(n);
+            int t = 0;
+            dfs(i,t,vis,adj);
+            ans = max(ans,t);
+        }
+        
+        return ans;
     }
 };
