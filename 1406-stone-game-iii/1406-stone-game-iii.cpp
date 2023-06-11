f3a8463a44c3c1d85a30ead41d0ddbf0ed612111
Arin Agarwal <arin.agarwal02@gmail.com>
2023-06-11 22:21:46 +0530

    Time: 328 ms (26.85%), Space: 136.9 MB (42.22%) - LeetHub

--- 1406-stone-game-iii/1406-stone-game-iii.cpp
+++ 1406-stone-game-iii/1406-stone-game-iii.cpp
@@ -1,28 +1,37 @@
 class Solution {
 public:
-    string stoneGameIII(vector<int>& stoneValue) 
-    {
-       int n = stoneValue.size(); 
-       vector<int> dp(3, 0); 
-
-       for (int i = n - 1; i >= 0; i--) {
-            int takeOne = stoneValue[i] - dp[(i + 1) % 3];
-
-            int takeTwo = INT_MIN;
-            if (i + 1 < n) takeTwo = stoneValue[i] + stoneValue[i + 1] - dp[(i + 2) % 3];
-
-            int takeThree = INT_MIN;
-            if (i + 2 < n) takeThree = stoneValue[i] + stoneValue[i + 1] + stoneValue[i + 2] - dp[(i + 3) % 3];
-
-            dp[i % 3] = max({takeOne, takeTwo, takeThree});
-        } 
-
-       int value = dp[0]; 
-       if(value > 0)
-           return "Alice";
-        else if(value  < 0)
+    vector<int> dp;
+    
+    int solve(int ind, vector<int> &nums){
+        if(ind >= nums.size())
+            return 0;
+        
+        if(dp[ind] != -1)
+            return dp[ind];
+        
+        int ans = INT_MIN, temp = 0;
+        for(int i = 0; i<3; i++){
+            if(i+ind < nums.size()){
+                temp += nums[i+ind];
+                ans = max(ans,temp-solve(i+ind+1,nums));
+            }
+        }
+        
+        return dp[ind] = ans;
+    }
+    
+    string stoneGameIII(vector<int>& stoneValue){
+        dp.resize(stoneValue.size(),-1);
+        int ans =  solve(0,stoneValue);
+        
+        if(ans > 0){
+            return "Alice";
+        }
+        else if(ans<0){
             return "Bob";
-        else
+        }
+        else{
             return "Tie";
+        }
     }
 };
