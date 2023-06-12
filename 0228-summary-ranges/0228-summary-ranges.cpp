1fd2a345ca040aeb186734af8881d280d8c78013
Arin Agarwal <arin.agarwal02@gmail.com>
2023-06-12 09:19:37 +0530

    Time: 0 ms (100.00%), Space: 6.9 MB (57.22%) - LeetHub

--- 0228-summary-ranges/0228-summary-ranges.cpp
+++ 0228-summary-ranges/0228-summary-ranges.cpp
@@ -6,31 +6,29 @@ public:
         if(nums.size() == 0)
             return ans;
         
-        int prev = nums[0], flag = 0; string temp = to_string(nums[0]);
+        int small = nums[0], large = nums[0];
+        int n = nums.size();
+        bool flag = 0;
         
-        for(int  i = 1; i<nums.size(); i++){
-            if(prev+1 == nums[i]){
-                flag = 1;
+        for(int i = 0;i<n-1; i++){
+            if(nums[i]+1 == nums[i+1]){
+                large = nums[i+1];
             }
             else{
-                if(flag){
-                    ans.push_back(temp+"->"+to_string(prev));
-                    flag = 0;
-                }
-                else{
-                    ans.push_back(temp);
-                }
+                if(small != large)
+                    ans.push_back(to_string(small)+"->"+to_string(large));
+                else
+                    ans.push_back(to_string(small));
                 
-                temp = to_string(nums[i]);
+                small = large = nums[i+1];
+                if(i == n-2) flag = 1;
             }
-            prev = nums[i];
         }
         
-        if(flag)
-            ans.push_back(temp+"->"+to_string(prev));
-        else
-            ans.push_back(temp);
-        
+            if(small != large)
+                    ans.push_back(to_string(small)+"->"+to_string(large));
+                else
+                    ans.push_back(to_string(small));        
         return ans;
     }
 };
