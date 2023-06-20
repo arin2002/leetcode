407c07b1fc8c146a88b8b0f7c2a6b0d2e042ce7a
Arin Agarwal <arin.agarwal02@gmail.com>
2023-06-20 08:24:41 +0530

    Time: 806 ms (5.23%), Space: 129.9 MB (79.40%) - LeetHub

---
+++ 2090-k-radius-subarray-averages/2090-k-radius-subarray-averages.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<int> getAverages(vector<int>& nums, int k) {
+        int n = nums.size();
+        vector<int> ans(n,-1);
+        
+        long long sum = 0;
+        for(int i = 0; i<2*k && i<n; i++){
+            sum += (long long)nums[i];
+        }
+        
+        int K = k, j = 0;
+        for(int i = 2*k; i<n; i++){
+            sum += (long long)nums[i];
+            cout<<sum<<" ";
+            ans[K++] = sum/(2*k+1);
+            sum-=nums[j++];
+        }
+        
+        return ans;
+    }
+};
