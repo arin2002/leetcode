4f5822aac1cf4b0a9103a3466e71092c2ad9cbf2
Arin Agarwal <arin.agarwal02@gmail.com>
2023-07-01 09:44:52 +0530

    Time: 455 ms (45.25%), Space: 162.7 MB (78.93%) - LeetHub

--- 1569-number-of-ways-to-reorder-array-to-get-same-bst/1569-number-of-ways-to-reorder-array-to-get-same-bst.cpp
+++ 1569-number-of-ways-to-reorder-array-to-get-same-bst/1569-number-of-ways-to-reorder-array-to-get-same-bst.cpp
@@ -1,32 +1,50 @@
 class Solution {
 public:
+    
     typedef long long ll;
-    ll MOD = 1e9 + 7;    
-    vector<vector<ll>> PT;
-
-    int solve(vector<int>& nums) {
-        int m = nums.size();        
-        if(m < 3) return 1;
-
-        vector<int> left, right;        
-        int root = nums[0];
-        for(int i = 1; i < m; i++)
-        {            
-            if(nums[i] < root) left.push_back(nums[i]);
-            else right.push_back(nums[i]);            
-        }        
-        ll leftways  = solve(left)  % MOD;
-        ll rightways = solve(right) % MOD;        
-        return (((leftways * rightways)%MOD) * PT[m-1][left.size()]) % MOD;        
+    ll mod = 1e9+7;
+    vector<vector<ll>> pascal;
+    
+    int solve(vector<int>& nums){
+        int n = nums.size();
+        
+        if(n < 3)
+            return 1;
+        
+        ll root = nums[0];
+        vector<int> left,right;
+        for(int i = 1; i<n; i++){
+            if(nums[i]>root){
+                right.push_back(nums[i]);
+            }
+            else{
+                left.push_back(nums[i]);
+            }
+        }
+        
+        // find now for these part arrangment
+        // realtive ordeting is done same
+        ll x = solve(left);
+        ll y = solve(right);
+        
+        // for current
+        ll z = pascal[n-1][left.size()];
+        return (x%mod*y%mod*z%mod)%mod;
     }
     
     int numOfWays(vector<int>& nums) {
-        int n = nums.size();        
-        PT.resize(n+1);        
-        for(int i = 0; i <= n; i++) {            
-            PT[i] = vector<long long>(i+1, 1);            
-            for(int j = 1; j < i; j++) PT[i][j] = (PT[i-1][j-1] + PT[i-1][j]) % MOD;
-        }        
-        return (solve(nums)-1) % MOD;
+        // maths questions can say
+        int n = nums.size();
+        
+        pascal.resize(n+1);
+        
+        for(int i = 0; i<n; i++){
+            pascal[i] = vector<ll>(i+1,1);
+            for(int j = 1; j<i; j++){
+                pascal[i][j] = (pascal[i-1][j]%mod+pascal[i-1][j-1]%mod)%mod; 
+            }
+        }
+        
+        return solve(nums)-1;
     }
 };
