61190aa589486655e7cdb52460c590e34152d0a2
Arin Agarwal <arin.agarwal02@gmail.com>
2023-07-04 16:23:34 +0530

    Time: 1800 ms (42.07%), Space: 6.8 MB (48.56%) - LeetHub

--- 2305-fair-distribution-of-cookies/2305-fair-distribution-of-cookies.cpp
+++ 2305-fair-distribution-of-cookies/2305-fair-distribution-of-cookies.cpp
@@ -1,38 +1,31 @@
 class Solution {
 public:
-    int dfs(int i, vector<int>& distribute, vector<int>& cookies, int k, int zeroCount) {
-        // If there are not enough cookies remaining, return INT_MAX 
-        // as it leads to an invalid distribution.
-        if (cookies.size() - i < zeroCount) {
-            return INT_MAX;
-        }
-
-        // After distributing all cookies, return the unfairness of this
-        // distribution.
-        if (i == cookies.size()) {
-            return *max_element(distribute.begin(), distribute.end());
-        }
-
-        // Try to distribute the i-th cookie to each child, and update answer
-        // as the minimum unfairness in these distributions.
-        int answer = INT_MAX;
-        for (int j = 0; j < k; ++j) {
-            zeroCount -= distribute[j] == 0 ? 1 : 0;
-            distribute[j] += cookies[i];
+    vector<int> nums;
+    int k;
+    
+    int solve(int i, vector<int>& cookies){
+        if(i == cookies.size()){
+            int maxi = INT_MIN;
+            for(auto &it: nums){
+                maxi = max(maxi,it);
+            }
             
-            // Recursively distribute the next cookie.
-            answer = min(answer, dfs(i + 1, distribute, cookies, k, zeroCount)); 
-            
-            distribute[j] -= cookies[i];
-            zeroCount += distribute[j] == 0 ? 1 : 0;
+            return maxi;
         }
         
-        return answer;
+        int ans = INT_MAX;
+        for(int j = 0; j<k; j++){
+            nums[j] += cookies[i];
+            ans = min(ans,solve(i+1,cookies));
+            nums[j] -= cookies[i];
+        }
+        
+        return ans;
     }
     
     int distributeCookies(vector<int>& cookies, int k) {
-        vector<int> distribute(k, 0);
-
-        return dfs(0, distribute, cookies, k, k);
+        nums.resize(k);
+        this->k = k;
+        return solve(0,cookies);
     }
 };
