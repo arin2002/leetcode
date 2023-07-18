b30599142b43a33ed20f0be1719f55ddf416725a
Arin Agarwal <arin.agarwal02@gmail.com>
2023-07-18 20:07:23 +0530

    Time: 523 ms (42.00%), Space: 173.9 MB (57.41%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.cpp
+++ 0146-lru-cache/0146-lru-cache.cpp
@@ -1,59 +1,70 @@
 class LRUCache {
 public:
-    list<int> dll; // doubly linked list in c++
-    int n;
-    // this is used for getting key and removing the
-    // element in order 1 time
-    unordered_map<int,pair<list<int>::iterator,int>> ump;
-    
+    list<int> dll; // Doubly linked list to keep track of the order of keys (from most recently used to least recently used).
+    int n; // Capacity of the LRUCache.
+    // unordered_map to store the key-value pair along with its position in the list.
+    // The value is a pair containing the iterator to the corresponding node in the list and the value associated with the key.
+    unordered_map<int, pair<list<int>::iterator, int>> ump;
+
     LRUCache(int capacity) {
         n = capacity;
     }
-    
+
     int get(int key) {
-        if(ump.find(key) == ump.end())
-            return -1;
-        
-        // if found we need to move it to first and update
-        auto it = ump[key].first;
-        int ans = ump[key].second;
-        
-        // remove the node as not needed O(1)
+        // Check if the key is present in the LRUCache.
+        if (ump.find(key) == ump.end())
+            return -1; // Key not found, return -1.
+
+        // Key found, we need to move it to the front (most recently used) and update its position.
+        auto it = ump[key].first; // Get the iterator to the node corresponding to the key in the list.
+        int ans = ump[key].second; // Get the value associated with the key.
+
+        // Remove the node from the list (as it will be moved to the front).
         dll.erase(it);
-        // add to front -> O(1)
+        // Add the key to the front of the list (making it most recently used).
         dll.push_front(key);
+        // Update the iterator of the key in the unordered_map to point to the new position in the list.
         ump[key].first = dll.begin();
-        
-        return ans;
+
+        return ans; // Return the value associated with the key.
     }
-    
+
     void put(int key, int value) {
-        if(ump.find(key) != ump.end()){
-            ump[key].second = value;
+        // Check if the key is already present in the LRUCache.
+        if (ump.find(key) != ump.end()) {
+            // Key exists, update its value and move it to the front (most recently used).
+            ump[key].second = value; // Update the value associated with the key.
+
+            // Remove the node from the list (as it will be moved to the front).
             dll.erase(ump[key].first);
+            // Add the key to the front of the list (making it most recently used).
             dll.push_front(key);
+            // Update the iterator of the key in the unordered_map to point to the new position in the list.
             ump[key].first = dll.begin();
 
-            return;
+            return; // We're done here.
         }
-        
-        if(dll.size()<n){
+
+        // Key doesn't exist, we need to add a new key-value pair to the LRUCache.
+        if (dll.size() < n) {
+            // LRUCache has not reached its capacity, add the new key-value pair to the front of the list.
             dll.push_front(key);
-            ump[key] = {dll.begin(),value};
-        }
-        else{
-            int k = dll.back();
+            // Update the unordered_map with the iterator to the new node in the list and the corresponding value.
+            ump[key] = {dll.begin(), value};
+        } else {
+            // LRUCache is at its capacity, need to remove the least recently used element.
+
+            // Get the key of the least recently used element from the back of the list.
+            int lruKey = dll.back();
+            // Remove the least recently used element from the list.
             dll.pop_back();
+            // Remove the least recently used element from the unordered_map as well.
+            ump.erase(lruKey);
+
+            // Add the new key-value pair to the front of the list.
             dll.push_front(key);
-            ump[key] = {dll.begin(),value};
-            ump.erase(k); 
+            // Update the unordered_map with the iterator to the new node in the list and the corresponding value.
+            ump[key] = {dll.begin(), value};
         }
     }
 };
-
-/**
- * Your LRUCache object will be instantiated and called as such:
- * LRUCache* obj = new LRUCache(capacity);
- * int param_1 = obj->get(key);
- * obj->put(key,value);
- */
