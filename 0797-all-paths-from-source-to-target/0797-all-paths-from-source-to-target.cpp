536f8e04c465bb36de81595561b8506489e0d2b7
Arin Agarwal <arin.agarwal02@gmail.com>
2023-07-20 22:53:14 +0530

    Time: 11 ms (87.40%), Space: 11.8 MB (55.18%) - LeetHub

--- 0797-all-paths-from-source-to-target/0797-all-paths-from-source-to-target.cpp
+++ 0797-all-paths-from-source-to-target/0797-all-paths-from-source-to-target.cpp
@@ -1,20 +1,32 @@
 class Solution {
 public:
-    vector<vector<int>> res; int n;
-    void dfs(vector<vector<int>>& graph, int currnode, vector<int>& temp) {
-        if(currnode == n-1) {
-            res.push_back(temp); return;
-        }        
-        for(int& child : graph[currnode]) {
-            temp.push_back(child);
-            dfs(graph, child, temp);
-            temp.pop_back();
+    vector<vector<int>> ans;
+    vector<int> temp;
+    vector<int> vis;
+    int n;
+    
+    void solve(int ind, vector<vector<int>>& graph){
+        if(ind == n-1){
+            ans.push_back(temp);
+            return;
         }
+        
+        vis[ind] = 1;
+        for(auto it: graph[ind]){
+            if(!vis[it]){
+                temp.push_back(it);
+                solve(it,graph);
+                temp.pop_back();
+            }
+        }
+        vis[ind] = 0;
     }
+    
     vector<vector<int>> allPathsSourceTarget(vector<vector<int>>& graph) {
         n = graph.size();
-        vector<int> temp = {0};
-        dfs(graph, 0, temp);
-        return res;
+        vis.resize(n);
+        temp.push_back(0);
+        solve(0,graph);
+        return ans;
     }
 };
