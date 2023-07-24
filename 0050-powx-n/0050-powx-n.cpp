c8e936f2f99e75ad564b291d1ec17faa2ec9c98d
Arin Agarwal <arin.agarwal02@gmail.com>
2023-07-24 10:05:19 +0530

    Time: 0 ms (100.00%), Space: 6 MB (28.53%) - LeetHub

---
+++ 0050-powx-n/0050-powx-n.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    double myPow(double x, int n) {
+        double res = 1;
+        if(n<0){
+            n = abs(n);
+            x = 1/x;
+        }
+        
+        while(n>0){
+            if(n&1){
+                res *= x;
+            }
+            x = x*x;
+            n >>= 1;
+        }
+        
+        return res;
+    }
+};
