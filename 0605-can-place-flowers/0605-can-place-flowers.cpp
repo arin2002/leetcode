d7a88fcdb434461738a1cf67598c1b3ad05e13c2
Arin Agarwal <arin.agarwal02@gmail.com>
2023-07-24 22:50:00 +0530

    Time: 11 ms (97.78%), Space: 20.5 MB (5.99%) - LeetHub

--- 0605-can-place-flowers/0605-can-place-flowers.cpp
+++ 0605-can-place-flowers/0605-can-place-flowers.cpp
@@ -1,49 +1,35 @@
-class Solution
-{
-    public:
-        bool canPlaceFlowers(vector<int> &nums, int m)
-        {
-            int n = nums.size();
-
-            if (m == 0)
-                return true;
-
-            if (n == 1 && nums[0] == 0) return true;
-
-            for (int i = 0; i < n; i++)
-            {
-                if (nums[i] == 0)
-                {
-                    if (i == 0)
-                    {
-                        if (nums[i + 1] == 0)
-                        {
-                            m--;
-                            nums[i] = 1;
-                        }
-                    }
-                    else if (i == n - 1)
-                    {
-                        if (nums[i - 1] == 0)
-                        {
-                            m--;
-                            nums[i] = 1;
-                        }
-                    }
-                    else
-                    {
-                        if (nums[i - 1] == 0 && nums[i + 1] == 0)
-                        {
-                            m--;
-                            nums[i] = 1;
-                        }
-                    }
+class Solution {
+public:
+    bool canPlaceFlowers(vector<int>& nums, int k) {
+        int n = nums.size();
+        
+        if(n == 1){
+            if(nums[0] == 0)
+                k--;
+            return k <= 0;
+        }
+        
+        for(int i = 0; i<n; i++){
+            if(i == 0){
+                if(nums[i] == 0 && nums[i+1]==0){
+                    nums[i] = 1;
+                    k--;
                 }
             }
-            //cout << m;
-            if (m <= 0)
-                return true;
-
-            return false;
+            else if(i != n-1 && nums[i] == 0){
+                if(nums[i-1] == 0 &&  nums[i+1] == 0){
+                    nums[i] = 1;
+                    k--;
+                }
+            }
+            else{
+                if(nums[i] == 0 && nums[i-1] == 0){
+                    nums[i] = 1;
+                    k--;
+                }
+            }
         }
+        
+        return k<=0;
+    }
 };
