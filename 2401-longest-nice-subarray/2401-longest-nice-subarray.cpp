f4a54ead8efd5dc9e58dd12cd92bf0e12d9075c3
Arin Agarwal <arin.agarwal02@gmail.com>
2023-07-26 20:09:30 +0530

    Time: 136 ms (89.34%), Space: 57.1 MB (55.90%) - LeetHub

--- 2401-longest-nice-subarray/2401-longest-nice-subarray.cpp
+++ 2401-longest-nice-subarray/2401-longest-nice-subarray.cpp
@@ -1,48 +1,53 @@
 class Solution {
 public:
     
-    void add(int n, vector<int> &freq){
-        // add set bits
-        for(int i = 0; i<31; i++){
-            int set = n&(1<<i);
-            if(set){
-                freq[i]++;
-            }
-        }
-    }
+//     void add(int n, vector<int> &freq){
+//         // add set bits
+//         for(int i = 0; i<31; i++){
+//             int set = n&(1<<i);
+//             if(set){
+//                 freq[i]++;
+//             }
+//         }
+//     }
     
-    void remove(int n, vector<int> &freq){
-        // add set bits
-        for(int i = 0; i<31; i++){
-            int set = n&(1<<i);
-            if(set){
-                freq[i]--;
-            }
-        }
-    }
+//     void remove(int n, vector<int> &freq){
+//         // remove set bits
+//         for(int i = 0; i<31; i++){
+//             int set = n&(1<<i);
+//             if(set){
+//                 freq[i]--;
+//             }
+//         }
+//     }
     
-    bool check(vector<int> &freq){
-        for(int i = 0; i<31; i++){
-            if(freq[i]>1){
-                return true;
-            }
-        }
+//     bool check(vector<int> &freq){
+//         for(int i = 0; i<31; i++){
+//             if(freq[i]>1){
+//                 return true;
+//             }
+//         }
         
-        return false;
-    }
+//         return false;
+//     }
     
     int longestNiceSubarray(vector<int>& nums) {
         int ans = 0, start = 0;
         int n = nums.size();
-        vector<int> freq(32);
+        // vector<int> freq(32);
+        int used = 0;
         
         for(int i = 0; i<n; i++){
-            add(nums[i],freq);
+            // add(nums[i],freq);
+            // used |= nums[i];
             
-            while(check(freq)){
-                remove(nums[start++],freq);
+            while(((used&nums[i]) != 0)){
+                // remove(nums[start++],freq);
+                used ^= nums[start++];
+                // same as doing -ve
             }
-            
+            used |= nums[i];
+            // same as adding
             ans = max(ans,i-start+1);
         }
         
@@ -51,22 +56,3 @@ public:
     
     }
 };
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
