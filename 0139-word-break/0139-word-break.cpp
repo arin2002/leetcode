e3c28781680821a3a7a92416a3cccbc00ca5fcfa
Arin Agarwal <arin.agarwal02@gmail.com>
2023-08-04 20:01:32 +0530

    Time: 3 ms (91.38%), Space: 8.4 MB (73.60%) - LeetHub

--- 0139-word-break/0139-word-break.cpp
+++ 0139-word-break/0139-word-break.cpp
@@ -1,40 +1,31 @@
 class Solution {
 public:
-    
-    vector<int> dp;
-    
-    bool solve(int i, unordered_set<string> &st, string &s){
-        if(i == s.size())
-            return 1;
-        
-        if(dp[i] != -1)
-            return dp[i];
-        
-        string word = "";
-        for(int j = i; j<s.size(); j++){
-            word += s[j];
+    bool wordBreak(string s, vector<string>& w) {
+        unordered_set<string> st(w.begin(),w.end());
+        int n = s.size();
+        vector<int> dp(n,-1);
+        function<int(int)>solve = [&](int ind){
             
-            // har jagye se partiton ho raha isme
-            // ek side se karoge to saari possiblity muskil h aana
-            // size dekh leta dp lagini the
-            if(st.find(word) != st.end()){
-                if(solve(j+1,st,s)){
-                    return dp[i] = 1;
+            if(ind == n)
+                return 1;
+            
+            if(dp[ind] != -1)
+                return dp[ind];
+            
+            string t;
+            for(int i = ind; i<n; i++){
+                t += s[i];
+                
+                if(st.find(t) != st.end()){
+                    if(solve(i+1)){
+                        return dp[ind] = 1;
+                    }
                 }
             }
-        }
+            
+            return dp[ind] = 0;
+        };
         
-        return dp[i] = 0;
-    }
-    
-    bool wordBreak(string s, vector<string>& wordDict) {
-        unordered_set<string> st;
-        int n = s.size();
-        
-        dp.resize(n,-1);
-        for(auto &it: wordDict)
-            st.insert(it);
-        
-        return solve(0,st,s);
+        return solve(0);
     }
 };
