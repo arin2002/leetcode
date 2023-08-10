bcb482df48015378841f842b7854c85fdd8e2f7b
Arin Agarwal <arin.agarwal02@gmail.com>
2023-08-10 20:17:30 +0530

    Time: 3 ms (74.13%), Space: 11 MB (50.91%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -1,57 +1,29 @@
 class Solution {
 public:
-    int search(vector<int>& nums, int target) {
-        int n = nums.size();
-        
-        int i = 0, j = n-1;
-        
-        int mini = 0;
-        while(i<=j)
-        {
-            int mid = i + (j-i)/2;
-            
-            int prev = (mid+n-1)%n;
-            int next = (mid+1)%n;
-            
-            if(nums[mid] <= nums[prev] && nums[mid] <= nums[next]){
-                mini = mid;
-                break;
-            }
-            
-            else if(nums[mid] < nums[j])
-                j = mid -1;
-            else
-                i = mid+1;
-        }
-        
-        i = 0;
-        j = mini-1;
-        while(i<=j)
-        {
-            int mid = (i+j)/2;
-            
-            if(nums[mid] == target)
-                return mid;
-            
-            else if(nums[mid] < target)
-                i = mid+1;
-            else
-                j = mid-1;
-        }
-        
-        i = mini; j = n-1;
-        while(i<=j)
-        {
-            int mid = (i+j)/2;
-            
-            if(nums[mid] == target)
-                return mid;
-            
-            else if(nums[mid] < target)
-                i = mid+1;
-            else
-                j = mid-1;
-        }
-        return -1;
+
+    int findpivot(int s, int e,vector<int>&nums){
+        if(s>e)return 0;
+        int mid=(s+e)/2;
+        if(s==e)return mid;
+        if(nums[mid]<nums[e])return findpivot(s,mid,nums);
+        return findpivot(mid+1,e,nums);
     }
+
+    int bs(int s,int e,vector<int>&nums,int tgt){
+        if(s>e)return -1;
+        int mid=(s+e)/2;
+        if(s==e&&nums[s]==tgt)return mid;
+        if(nums[mid]==tgt)return mid;
+        else if(nums[mid]>tgt)return bs(s,mid-1,nums,tgt);
+        return bs(mid+1,e,nums,tgt);
+    }
+
+    int search(vector<int>& nums, int target) {
+        int s=0,e=nums.size()-1;
+        int pivot=findpivot(s,e,nums);
+        int a=bs(s,pivot-1,nums,target);
+        if(a!=-1)return a;
+        return bs(pivot,e,nums,target);
+    }
+    
 };
