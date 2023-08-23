2cb73a38174f64bbd2c15f2fa1adede8ddad747e
Arin Agarwal <arin.agarwal02@gmail.com>
2023-08-23 18:23:55 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (46.07%) - LeetHub

--- 0767-reorganize-string/0767-reorganize-string.cpp
+++ 0767-reorganize-string/0767-reorganize-string.cpp
@@ -4,6 +4,7 @@ public:
         int n = s.size();
         priority_queue<pair<int,char>> pq;
         unordered_map<char,int> ump;
+        int maxi = 0; char c = 0;
         
         for(auto &it: s){
             ump[it]++;
@@ -13,34 +14,60 @@ public:
             // this condition is the main part
             if(check > (n+1)/2)
                 return "";
+            
+            if(check > maxi){
+                maxi = check;
+                c = it;
+            }
+        }
+        
+        // for(auto [a,b] : ump){
+        //     pq.push({b,a});
+        // }
+        
+        string res = s;
+//         while(pq.size() >= 2){
+//             char a= pq.top().second;
+//             int c1= pq.top().first-1;
+//             pq.pop();
+            
+//             char b = pq.top().second;
+//             int c2 = pq.top().first-1;
+//             pq.pop();
+            
+//             res += a;
+//             res += b;
+            
+//             if(c1 > 0)
+//                 pq.push({c1,a});
+            
+//             if(c2>0)
+//                 pq.push({c2,b});
+//         }
+        
+//         if(pq.size() == 1 && pq.top().first == 1)
+//             res += pq.top().second;
+        
+        // add max in even indices
+        int i = 0;
+        for(; ump[c]>0; i+=2){
+            res[i] = c;
+            ump[c]--;
         }
         
         for(auto [a,b] : ump){
-            pq.push({b,a});
+            if(b == 0)
+                continue;
+            
+            for(; b>0; i+=2){
+                
+                if(i>=n)
+                    i = 1;
+                
+                res[i] = a;
+                b--;
+            }
         }
-        
-        string res;
-        while(pq.size() >= 2){
-            char a= pq.top().second;
-            int c1= pq.top().first-1;
-            pq.pop();
-            
-            char b = pq.top().second;
-            int c2 = pq.top().first-1;
-            pq.pop();
-            
-            res += a;
-            res += b;
-            
-            if(c1 > 0)
-                pq.push({c1,a});
-            
-            if(c2>0)
-                pq.push({c2,b});
-        }
-        
-        if(pq.size() == 1 && pq.top().first == 1)
-            res += pq.top().second;
         
         return res;
     }
