4ad68b408b03850973e2bd1d29839cbf4ce712a6
Arin Agarwal <arin.agarwal02@gmail.com>
2023-08-25 19:54:45 +0530

    Time: 4 ms (70.98%), Space: 7.1 MB (50.41%) - LeetHub

--- 0097-interleaving-string/0097-interleaving-string.cpp
+++ 0097-interleaving-string/0097-interleaving-string.cpp
@@ -3,33 +3,37 @@ public:
     bool isInterleave(string s1, string s2, string s3) {
         int n = s1.size(), m = s2.size(), mn = s3.size();
 
-        if (n + m != mn)
-            return false;
-
-        vector<vector<vector<int>>> dp(n + 1, vector<vector<int>>(m + 1, vector<int>(mn + 1, -1)));
-
-        function<int(int, int, int)> solve = [&](int i, int j, int k) {
-            if (i == n && j == m && k == mn) {
+        if(n + m != mn)
+            return 0;
+        
+        // vector<vector<vector<int>>> dp(n+1, vector<vector<int>>(m+1, vector<int>(mn+1, -1)));
+        vector<vector<int>> dp(n+1,vector<int>(m+1,-1));
+        
+        function<int(int,int,int)>solve = [&](int i, int j, int k){
+            
+            // cout<<i<<" "<<j<<" "<<k<<" "<<endl;
+            if(i == n && j == m && k == mn){
                 return 1;
             }
-
-            if (k == mn)
+            
+            if(k == mn)
                 return 0;
-
-            if (dp[i][j][k] != -1)
-                return dp[i][j][k];
-
-            bool l = false, r = false;
-
-            if (i < n && s1[i] == s3[k])
-                l = solve(i + 1, j, k + 1);
-
-            if (j < m && s2[j] == s3[k])
-                r = solve(i, j + 1, k + 1);
-
-            return dp[i][j][k] = l | r;
+            
+            if(dp[i][j]!= -1)
+                return dp[i][j];
+            
+            bool l = 0,r = 0;
+            
+            if(s1[i] == s3[k])
+                l = solve(i+1,j,k+1);
+            
+            if(s2[j] == s3[k]){
+                r = solve(i,j+1,k+1);
+            }
+            
+            return dp[i][j] = l|r;
         };
-
-        return solve(0, 0, 0);
+        
+        return solve(0,0,0);
     }
 };
