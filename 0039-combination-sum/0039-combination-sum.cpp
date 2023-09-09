cfd502a6ef972e412c0a66d60b548837611c1af7
Arin Agarwal <arin.agarwal02@gmail.com>
2023-09-09 10:56:47 +0530

    Time: 0 ms (100.00%), Space: 17.2 MB (28.54%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.cpp
+++ 0039-combination-sum/0039-combination-sum.cpp
@@ -1,26 +1,30 @@
 class Solution {
-public:    
-    set<vector<int>> s;
-    vector<int> temp;
-    
-    void dfs(int ind, vector<int>& nums, int target){
-        if(ind >= nums.size()){
-            if(target == 0)
-                s.insert(temp);
-            return;
-        }
-        if(target >= nums[ind]){
-            temp.push_back(nums[ind]);
-            dfs(ind,nums,target-nums[ind]);
-            temp.pop_back();
-        }
+public:
+    vector<vector<int>> combinationSum(vector<int>& nums, int target) {
+        vector<vector<int>> ans;
+        vector<int> temp;
+        int n = nums.size();
         
-        dfs(ind+1,nums,target);
-    }
-    
-    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
-        dfs(0,candidates,target);
-        vector<vector<int>> ans(s.begin(),s.end());
+        function<void(int,int)>solve = [&](int ind, int k){
+            if(ind  == n){
+                if(k == 0){
+                    ans.push_back(temp);
+                }
+                
+                return;
+            }
+            
+            if(nums[ind] <= k){
+                temp.push_back(nums[ind]);
+                solve(ind,k-nums[ind]);
+                temp.pop_back();
+            }
+            
+            solve(ind+1,k);
+            
+        };
+        
+        solve(0,target);
         return ans;
     }
 };
