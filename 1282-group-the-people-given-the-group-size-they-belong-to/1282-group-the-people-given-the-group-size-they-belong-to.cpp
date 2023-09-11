997eb0d3aacb8a138485d2cbb636597a5b4002d6
Arin Agarwal <arin.agarwal02@gmail.com>
2023-09-11 10:50:44 +0530

    Time: 11 ms (57.70%), Space: 13.9 MB (21.49%) - LeetHub

---
+++ 1282-group-the-people-given-the-group-size-they-belong-to/1282-group-the-people-given-the-group-size-they-belong-to.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    vector<vector<int>> groupThePeople(vector<int>& nums) {
+        vector<vector<int>> ans;
+        unordered_map<int,vector<int>> ump;
+        int n = nums.size();
+        
+        for(int i = 0; i<n; i++){
+            ump[nums[i]].push_back(i);
+        }
+        
+        for(auto [a,b]: ump){
+            int size = b.size();
+            
+            int i = 0, m = a;
+            while(size){
+                vector<int> temp;
+                cout<<m<<" "<<size<<endl;
+                for(int j = 0; j<m; j++,size--){
+                    temp.push_back(b[i++]);
+                }
+                
+                ans.push_back(temp);
+            }
+        }
+        
+        return ans;
+    }
+};
