8fcc2635bcc7d33c5e2fba7393a66d5631f21d9c
Arin Agarwal <arin.agarwal02@gmail.com>
2023-09-12 10:35:18 +0530

    Time: 51 ms (74.86%), Space: 17.2 MB (92.40%) - LeetHub

--- 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.cpp
+++ 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.cpp
@@ -4,31 +4,30 @@ public:
         // int freq[27] = {0};
         vector<int> freq(26);
         // priority_queue<int,vector<int>,greater<int>> pq;
-        priority_queue<int> pq;
+        // priority_queue<int> pq;
         int ans = 0;
         
         for(auto &it: s){
             freq[it-'a']++;
         }
         
-        for(auto it: freq){
-            if(it > 0)
-                pq.push(it);
-        }
+        sort(freq.begin(),freq.end());
         
-        while(pq.size()>1){
-            int a = pq.top();
-            pq.pop();
-            int b = pq.top();
+        for(int i = 24; i>=0; i--){
+            if(freq[i] == 0)
+                break;
             
-            // cout<<a<<" "<<b<<endl;
-            if(a == b){
-                a--;
-                ans++;
-                if(a > 0)
-                    pq.push(a);
+            // we are trying to make ascending order
+            // so >=  done to make it possible
+            if(freq[i] >= freq[i+1]){
+                int prev = freq[i];
+                freq[i] = max(0, freq[i+1]-1);
+                // change
+                ans += prev-freq[i];
             }
         }
+        
+        
         
         return ans;
     }
