51c8c8b94f4242d39ce219c78d9effc38f99a1ac
Arin Agarwal <arin.agarwal02@gmail.com>
2023-09-13 22:03:25 +0530

    Time: 21 ms (37.33%), Space: 18.2 MB (15.54%) - LeetHub

--- 0135-candy/0135-candy.cpp
+++ 0135-candy/0135-candy.cpp
@@ -1,25 +1,28 @@
 class Solution {
 public:
     int candy(vector<int>& nums) {
-        int n = nums.size(), ans = 0;
-        vector<int> pre(n,1), suff(n,1);
+        int n = nums.size(), res = 0;
+        // vector<int> pre(n,1), suff(n,1);
+        vector<int> ans(n,1);
         
         for(int i = 1; i<n; i++){
             if(nums[i] > nums[i-1]){
-                pre[i] = pre[i-1]+1;
+                // pre[i] = pre[i-1]+1;
+                ans[i] = max(ans[i],ans[i-1]+1);
             }
         }
         
         for(int i = n-2; i>=0; i--){
             if(nums[i] > nums[i+1]){
-                suff[i] = suff[i+1]+1;
+                // suff[i] = suff[i+1]+1;
+                ans[i] = max(ans[i],ans[i+1]+1);
             }
         }
         
         for(int i = 0; i<n; i++){
-            ans += max(pre[i],suff[i]);
+            res += ans[i];
         }
         
-        return ans;
+        return res;
     }
 };
