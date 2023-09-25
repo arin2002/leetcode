ddb6500a34d906a95f3415c496a0c8daa85f82bf
Arin Agarwal <arin.agarwal02@gmail.com>
2023-09-25 10:13:33 +0530

    Time: 0 ms (100.00%), Space: 6.9 MB (25.22%) - LeetHub

--- 0389-find-the-difference/0389-find-the-difference.cpp
+++ 0389-find-the-difference/0389-find-the-difference.cpp
@@ -1,22 +1,20 @@
 class Solution {
 public:
     char findTheDifference(string s, string t) {
-        vector<int> v1(27,0);
-        vector<int> v2(27,0);
-        
-        for(auto it : s)
-            v1[it-'a']++;
-        
-        for(auto it : t){
-            v2[it-'a']++;
-
+        vector<int> v(26);
+        int n = s.size(), m = t.size();
+        for(int i = 0; i<n; i++){
+            v[s[i]-'a']++;
         }
         
-        for(auto it: t){
-            if(v1[it-'a'] != v2[it-'a'])
-                return it;
+        for(int i = 0; i<m; i++){
+            v[t[i]-'a']--;
+            
+            if(v[t[i]-'a'] < 0){
+                return t[i];
+            }
         }
         
-        return 'c';
+        return '-1';
     }
 };
