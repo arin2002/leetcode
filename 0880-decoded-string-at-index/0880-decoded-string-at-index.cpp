1d67b3f2327ba9233594b854ed362ef1556d9d53
Arin Agarwal <arin.agarwal02@gmail.com>
2023-09-27 18:21:09 +0530

    Time: 3 ms (9.11%), Space: 6.4 MB (11.71%) - LeetHub

---
+++ 0880-decoded-string-at-index/0880-decoded-string-at-index.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    string decodeAtIndex(string s, int k) {
+        long long  cnt=1;
+        int j=0;
+        for(int i=1;i<s.size();i++){
+            if(cnt>=k){
+                break;
+            }
+            if(s[i]<='9'&&s[i]>='1'){
+                cnt=cnt*(s[i]-'0');
+            }
+            else{
+                cnt++;
+            }
+            j++;
+        }
+        int i=j;
+        while(i>=0){
+            if(s[i]<='9'&&s[i]>='1'){
+                cnt=cnt/(s[i]-'0');
+            }
+            else{
+                if(k==0||k==cnt){
+                    break;
+                }
+                cnt--;
+            }
+            if(cnt<=k){
+                k=k%cnt;
+            }
+            i--;
+        }
+        string ans;
+        ans.push_back(s[i]);
+        return ans;
+    }
+};
