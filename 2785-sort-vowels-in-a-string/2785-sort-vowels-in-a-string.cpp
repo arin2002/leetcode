141bae96a5718b7a466c4876a99553459c7b271d
Arin Agarwal <arin.agarwal02@gmail.com>
2023-11-13 15:19:56 +0530

    Time: 54 ms (22.80%), Space: 13 MB (51.70%) - LeetHub

--- 2785-sort-vowels-in-a-string/2785-sort-vowels-in-a-string.cpp
+++ 2785-sort-vowels-in-a-string/2785-sort-vowels-in-a-string.cpp
@@ -3,19 +3,25 @@ public:
     string sortVowels(string s) {
         string t;
         unordered_set<char> st{'a','e','i','o','u','A','E','I','O','U'};
-        
+        unordered_map<char,int> ump;
         for(auto &it: s){
             if(st.find(it) != st.end()){
-                t += it;
+                ump[it]++;
             }
         }
         
-        sort(t.begin(),t.end());
-        int i = 0;
+        int j = 0;
+        string sorted = "AEIOUaeiou";
+        
         
         for(auto &it: s){
             if(st.find(it) != st.end()){
-                it = t[i++];
+                while(ump[sorted[j]] == 0){
+                    j++;
+                }
+                
+                it = sorted[j];
+                ump[sorted[j]]--; 
             }
         }
         
