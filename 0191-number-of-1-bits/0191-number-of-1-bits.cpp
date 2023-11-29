bf170a3aaf6553b62eda624830c23a81b2257393
Arin Agarwal <arin.agarwal02@gmail.com>
2023-11-29 15:23:17 +0530

    Time: 0 ms (100.00%), Space: 6.6 MB (9.00%) - LeetHub

--- 0191-number-of-1-bits/0191-number-of-1-bits.cpp
+++ 0191-number-of-1-bits/0191-number-of-1-bits.cpp
@@ -1,15 +1,15 @@
 class Solution {
 public:
     int hammingWeight(uint32_t n) {
+        int ans = 0;
         
-        int count = 0;
-        while(n!=0){
+        while(n){
             if(n&1)
-                count++;
+                ans++;
             
-            n>>=1;
+            n >>=1;
         }
         
-        return count;
+        return ans;
     }
 };
