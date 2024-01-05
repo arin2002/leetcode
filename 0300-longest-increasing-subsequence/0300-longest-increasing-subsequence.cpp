cb777f8752859ef715b6cfafd0186e82fc6d3c24
Arin Agarwal <arin.agarwal02@gmail.com>
2024-01-05 11:47:40 +0530

    Time: 4 ms (93.93%), Space: 10.9 MB (62.11%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -1,20 +1,18 @@
 class Solution {
 public:
     int lengthOfLIS(vector<int>& nums) {
+        int n = nums.size();
         vector<int> temp;
         temp.push_back(nums[0]);
         
-        for(int i = 1; i<nums.size(); i++){
-            if(temp.back()<nums[i]){
+        for(int i = 1; i<n; i++){
+            if(nums[i] > temp.back()){
                 temp.push_back(nums[i]);
+                continue;
             }
-            else{
-                //this is bs here now to replace from array
-                // as we need only lrngth
-                // like [10] is there in temp now we need to add 9
-                int ind = lower_bound(temp.begin(),temp.end(),nums[i])-temp.begin();
-                temp[ind] = nums[i];
-            }
+            
+            int ind = lower_bound(temp.begin(),temp.end(),nums[i])-temp.begin();
+            temp[ind] = nums[i];
         }
         
         return temp.size();
