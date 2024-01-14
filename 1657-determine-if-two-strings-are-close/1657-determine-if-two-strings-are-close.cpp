c74cedb859883cdbd348cba0371801206f5bbbf5
Arin Agarwal <arin.agarwal02@gmail.com>
2024-01-14 16:36:46 +0530

    Time: 99 ms (67.17%), Space: 21.2 MB (26.20%) - LeetHub

--- 1657-determine-if-two-strings-are-close/1657-determine-if-two-strings-are-close.cpp
+++ 1657-determine-if-two-strings-are-close/1657-determine-if-two-strings-are-close.cpp
@@ -1,37 +1,38 @@
 class Solution {
 public:
     bool closeStrings(string s, string t) {
-        int n=s.size(), m = t.size();
+        int n = s.size(), m = t.size();
         
-        if(n!=m)
+        if(n != m)
             return 0;
         
-        unordered_map<int,int> ump1,ump2;
-        for(int i = 0; i<n; i++){
-            ump1[s[i]]++;
-            ump2[t[i]]++;
+        /*
+             aabbcc
+             abbccc
+        */
+        // sort(s.begin(),s.end());
+        // sort(t.begin(),t.end());
+        
+        // if(s == t)
+        //     return 1;
+        
+        vector<int> v1(26), v2(26);
+        unordered_set<char> st(s.begin(),s.end());
+        
+        for(auto it: s){
+            v1[it-'a']++;
         }
         
-        for(int i = 0; i<n; i++){
-            if(ump1.find(t[i]) == ump1.end())
+        for(auto it: t){
+            if(st.find(it) == st.end())
                 return 0;
             
-            if(ump2.find(s[i]) == ump2.end())
-                return 0;
-        }
-        
-        vector<int> v1,v2;
-        for(auto [i,j] : ump1){
-            v1.push_back(j);
-        }
-        
-        for(auto [i,j] : ump2){
-            v2.push_back(j);
+            v2[it-'a']++;
         }
         
         sort(v1.begin(),v1.end());
         sort(v2.begin(),v2.end());
         
-        return v1==v2;
+        return v1 == v2;
     }
 };
