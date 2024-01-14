694b27c9911a250bf81aef2a2a7c34452678f26d
Arin Agarwal <arin.agarwal02@gmail.com>
2024-01-14 16:38:01 +0530

    Time: 102 ms (63.65%), Space: 20.9 MB (78.40%) - LeetHub

--- 1657-determine-if-two-strings-are-close/1657-determine-if-two-strings-are-close.cpp
+++ 1657-determine-if-two-strings-are-close/1657-determine-if-two-strings-are-close.cpp
@@ -6,28 +6,18 @@ public:
         if(n != m)
             return 0;
         
-        /*
-             aabbcc
-             abbccc
-        */
-        // sort(s.begin(),s.end());
-        // sort(t.begin(),t.end());
-        
-        // if(s == t)
-        //     return 1;
-        
         vector<int> v1(26), v2(26);
         unordered_set<char> st(s.begin(),s.end());
-        
-        for(auto it: s){
-            v1[it-'a']++;
-        }
         
         for(auto it: t){
             if(st.find(it) == st.end())
                 return 0;
             
             v2[it-'a']++;
+        }
+        
+        for(auto it: s){
+            v1[it-'a']++;
         }
         
         sort(v1.begin(),v1.end());
