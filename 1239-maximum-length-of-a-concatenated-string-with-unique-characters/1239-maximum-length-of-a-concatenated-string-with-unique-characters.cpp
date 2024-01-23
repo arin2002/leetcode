06165470e6afe5ac8a8eb6d3405350985d05416a
Arin Agarwal <arin.agarwal02@gmail.com>
2024-01-23 18:16:34 +0530

    Time: 876 ms (6.00%), Space: 335 MB (6.31%) - LeetHub

--- 1239-maximum-length-of-a-concatenated-string-with-unique-characters/1239-maximum-length-of-a-concatenated-string-with-unique-characters.cpp
+++ 1239-maximum-length-of-a-concatenated-string-with-unique-characters/1239-maximum-length-of-a-concatenated-string-with-unique-characters.cpp
@@ -1,57 +1,44 @@
 class Solution {
 public:
-    
-    bool cmp(string &s, vector<int> &v){
-        vector<int> self(26,0);
-        
-        for(int i = 0; i<s.length(); i++){
-            if(self[s[i]-'a'] == 1)
-                return true;
-            
-            self[s[i]-'a'] = 1;
+    void fun(int idx,vector<string>& arr,vector<string>& temp,int& maxlength)
+    {
+        if(idx==arr.size())
+        {
+            string s1="";
+            for(int i=0;i<temp.size();i++)
+            {
+                s1+=temp[i];
+            }
+            int freq[26]={0};
+            for(int i=0;i<s1.size();i++)
+            {
+                freq[s1[i]-'a']++;
+            }
+            int cnt=0;
+            for(int i=0;i<26;i++)
+            {
+                if(freq[i]==1)
+                {
+                    cnt++;
+                }
+                else if(freq[i]>1)
+                {
+                    cnt=0;
+                    return;
+                }
+            }
+            maxlength=max(maxlength,cnt);
+            return;
         }
-        
-        for(auto i : s){
-            if(v[i-'a'] == 1)
-                return true;
-        }
-        
-        return false;
+        temp.push_back(arr[idx]);
+        fun(idx+1,arr,temp,maxlength);
+        temp.pop_back();
+        fun(idx+1,arr,temp,maxlength);
     }
-    
-    
-    int f(int i, vector<string> &nums, vector<int> v, int len){
-        if(i<0)
-            return len;
-        
-        string s = nums[i];
-        if(cmp(nums[i],v))
-            return f(i-1,nums,v,len);
-        
-        for(auto j : s){
-            v[j-'a'] = 1;
-        }
-        
-        //pick
-        len += s.size();
-        int l = f(i-1,nums,v,len);
-        
-        for(auto j : s){
-            v[j-'a'] = 0;
-        }
-        
-        len -= s.size();
-        
-        int r = f(i-1,nums,v,len);
-        
-        return max(l,r);
-    }
-    
-    
     int maxLength(vector<string>& arr) {
-        int n = arr.size();
-        
-        vector<int> v(26,0);
-        return f(n-1,arr,v,0);
+        vector<string> temp;
+        int maxlength=0;
+        fun(0,arr,temp,maxlength);
+        return maxlength;
     }
 };
