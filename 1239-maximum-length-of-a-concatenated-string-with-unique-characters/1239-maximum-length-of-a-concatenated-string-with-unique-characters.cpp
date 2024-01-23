c0ceb2d6b22940744a355d48053c19b79e1d3146
Arin Agarwal <arin.agarwal02@gmail.com>
2024-01-23 18:23:10 +0530

    Time: 1270 ms (5.07%), Space: 463.9 MB (5.07%) - LeetHub

--- 1239-maximum-length-of-a-concatenated-string-with-unique-characters/1239-maximum-length-of-a-concatenated-string-with-unique-characters.cpp
+++ 1239-maximum-length-of-a-concatenated-string-with-unique-characters/1239-maximum-length-of-a-concatenated-string-with-unique-characters.cpp
@@ -1,44 +1,32 @@
 class Solution {
 public:
-    void fun(int idx,vector<string>& arr,vector<string>& temp,int& maxlength)
-    {
-        if(idx==arr.size())
-        {
-            string s1="";
-            for(int i=0;i<temp.size();i++)
-            {
-                s1+=temp[i];
-            }
-            int freq[26]={0};
-            for(int i=0;i<s1.size();i++)
-            {
-                freq[s1[i]-'a']++;
-            }
-            int cnt=0;
-            for(int i=0;i<26;i++)
-            {
-                if(freq[i]==1)
-                {
-                    cnt++;
+    int maxLength(vector<string>& nums) {
+        int n = nums.size();
+        int ans = 0;
+        string t;
+        
+        function<void(int, string)> solve = [&](int ind, string t){
+            if(ind == n){
+                // unordered_set<char> st;
+                vector<char> st(26);
+                
+                for(auto &it: t){
+                    
+                    st[it-'a']++;
+                    
+                    if(st[it-'a'] > 1)
+                        return;
                 }
-                else if(freq[i]>1)
-                {
-                    cnt=0;
-                    return;
-                }
+                
+                ans = max(ans,(int)t.size());
+                return;
             }
-            maxlength=max(maxlength,cnt);
-            return;
-        }
-        temp.push_back(arr[idx]);
-        fun(idx+1,arr,temp,maxlength);
-        temp.pop_back();
-        fun(idx+1,arr,temp,maxlength);
-    }
-    int maxLength(vector<string>& arr) {
-        vector<string> temp;
-        int maxlength=0;
-        fun(0,arr,temp,maxlength);
-        return maxlength;
+            
+            solve(ind+1,t+nums[ind]);
+            solve(ind+1,t);
+        };
+        
+        solve(0,t);
+        return ans;
     }
 };
