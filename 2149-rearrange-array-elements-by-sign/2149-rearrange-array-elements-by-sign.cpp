c179a0db344632e81cfccd5b1d268e8533389ec8
Arin Agarwal <arin.agarwal02@gmail.com>
2024-02-14 17:28:30 +0530

    Time: 131 ms (94.75%), Space: 126.3 MB (50.65%) - LeetHub

--- 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.cpp
+++ 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.cpp
@@ -1,35 +1,18 @@
 class Solution {
 public:
     vector<int> rearrangeArray(vector<int>& nums) {
-        vector<int> ans;
-        vector<pair<int,int>> neg,pos;
+        int i = 0, j = 1, n = nums.size();
+        vector<int> ans(n);
+        int pos = 0, neg = 1;
         
-        for(int i = 0; i<nums.size(); i++){
-            if(nums[i]<0){
-                neg.push_back({nums[i],i});
+        for(auto it: nums){
+            if(it<0){
+                ans[neg] = it; 
+                neg += 2;
             }
             else{
-                pos.push_back({nums[i],i});
-            }
-        }
-        
-        for(int i = 0; i<pos.size(); i++){
-            
-            if(i == 0){
-                ans.push_back(pos[i].first);
-                ans.push_back(neg[i].first);
-            }
-            else{
-                // cout<<neg[i].second<<" "<<pos[i].second<<" "<<neg[i].first<<" "<<pos[i].first<<endl;
-                
-                if(neg[i].second<pos[i].second && ans.back()>0){
-                    ans.push_back(neg[i].first);
-                    ans.push_back(pos[i].first);
-                }
-                else{
-                    ans.push_back(pos[i].first);
-                    ans.push_back(neg[i].first);
-                }
+                ans[pos] = it;
+                pos += 2;
             }
         }
         
