c962c10c863a6ee17a70007585255652c6821fd1
Arin Agarwal <arin.agarwal02@gmail.com>
2024-02-19 16:45:16 +0530

    Time: 5 ms (20.09%), Space: 7.2 MB (39.77%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.cpp
+++ 0231-power-of-two/0231-power-of-two.cpp
@@ -1,18 +1,9 @@
 class Solution {
 public:
     bool isPowerOfTwo(int n) {
-        
         if(n <= 0)
             return 0;
         
-        while(n>1){
-            if(n%2 == 1){
-                return 0;
-            }
-            
-            n /= 2;
-        }
-        
-        return 1;
+        return !(n&(n-1));
     }
 };
