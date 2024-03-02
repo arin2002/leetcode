0d41b78518f572aa7010d3521dea59413c24c094
Arin Agarwal <arin.agarwal02@gmail.com>
2024-03-02 13:45:17 +0530

    Time: 21 ms (73.33%), Space: 28.5 MB (46.74%) - LeetHub

--- 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.cpp
+++ 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.cpp
@@ -1,25 +1,42 @@
 class Solution {
 public:
     vector<int> sortedSquares(vector<int>& nums) {
-        int maxi = 0;
-        for(auto &it: nums){
-            it *= it;
-            maxi = max(it,maxi);
-        }
+        // int maxi = 0;
+        // for(auto &it: nums){
+        //     it *= it;
+        //     maxi = max(it,maxi);
+        // }
         
         // sort(nums.begin(),nums.end());
-        vector<int> v(maxi+1);
+//         vector<int> v(maxi+1);
         
-        for(auto &it: nums){
-            v[it]++;
-        }
+//         for(auto &it: nums){
+//             v[it]++;
+//         }
         
-        int j = 0;
-        for(int i = 0; i<=maxi; i++){
-            while(v[i]-->0){
-                nums[j++] = i;
+//         int j = 0;
+//         for(int i = 0; i<=maxi; i++){
+//             while(v[i]-->0){
+//                 nums[j++] = i;
+//             }
+//         }
+        int left = 0, n = nums.size(), right = n-1, ind = n-1;
+        vector<int> ans(n);
+        
+        while(left<=right){
+            int l = nums[left]*nums[left];
+            int r = nums[right]*nums[right];
+            
+            if(l<=r){
+                right--;
+                ans[ind--] = r;
+            }
+            else{
+                ans[ind--] = l;
+                left++;
             }
         }
-        return nums;
+        
+        return ans;
     }
 };
