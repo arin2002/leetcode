d0f3bfc474f4cbe520dcaa692fb815951b892231
Arin Agarwal <arin.agarwal02@gmail.com>
2024-03-13 11:46:19 +0530

    Time: 0 ms (100.00%), Space: 7.2 MB (41.86%) - LeetHub

--- 2485-find-the-pivot-integer/2485-find-the-pivot-integer.cpp
+++ 2485-find-the-pivot-integer/2485-find-the-pivot-integer.cpp
@@ -1,25 +1,21 @@
 class Solution {
 public:
-    
-    int f(int x){
-        return (x*(x+1))/2;
-    }
-    
     int pivotInteger(int n) {
-        int i = 1, j = n;
+        int l = 1, r = n, sum = ((n)*(n+1))/2;
         
-        int total = (n*(n+1))/2;
-        
-        while(i<=j){
-            int mid = (i+j)/2;
+        while(l<=r){
+            int mid = (l+r)/2;
+            int lsum = ((mid)*(mid+1))/2, rsum = sum-lsum+mid;
             
-            if(f(mid) == total-f(mid-1))
+            cout<<mid<<" ";
+            if(lsum == rsum)
                 return mid;
             
-            if(f(mid)>total-f(mid-1))
-                j = mid-1;
-            else
-                i = mid+1;
+            else if(lsum<rsum)
+                l = mid+1;
+            else{
+                r = mid-1;
+            }
         }
         
         return -1;
