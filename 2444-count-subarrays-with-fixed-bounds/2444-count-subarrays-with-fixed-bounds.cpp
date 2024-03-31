6940957f4966717b6d0b6d6c7c0c0267de14155a
Arin Agarwal <arin.agarwal02@gmail.com>
2024-03-31 19:04:06 +0530

    Time: 78 ms (93.35%), Space: 82.5 MB (53.99%) - LeetHub

--- 2444-count-subarrays-with-fixed-bounds/2444-count-subarrays-with-fixed-bounds.cpp
+++ 2444-count-subarrays-with-fixed-bounds/2444-count-subarrays-with-fixed-bounds.cpp
@@ -1,28 +1,21 @@
 class Solution {
 public:
     long long countSubarrays(vector<int>& nums, int minK, int maxK) {
-        int n = nums.size();
-        int minKpos = -1, maxKpos = -1, culprit = -1; 
-        
-        long long ans = 0;
-        for(int i = 0; i<n; i++){
-            if(nums[i]>maxK || nums[i]<minK){
-                // reset pointer as got max/min
-                culprit = i;
-                minKpos = -1;
-                maxKpos = -1;
-                continue;
+        long long res = 0;
+        int bad_idx = -1, left_idx = -1, right_idx = -1;
+
+        for (int i = 0; i < nums.size(); ++i) {
+            if (!(minK <= nums[i] && nums[i] <= maxK)) {
+                bad_idx = i;
             }
-            
-            if(nums[i] == maxK)
-                maxKpos = i;
-            
-            if(nums[i] == minK)
-                minKpos = i;
-            
-            // this is the main forumla one should come with
-            ans += max(0,min(maxKpos,minKpos)-culprit);
+            if (nums[i] == minK) {
+                left_idx = i;
+            }
+            if (nums[i] == maxK) {
+                right_idx = i;
+            }
+            res += max(0, min(left_idx, right_idx) - bad_idx);
         }
-        return ans;
+        return res;
     }
 };
