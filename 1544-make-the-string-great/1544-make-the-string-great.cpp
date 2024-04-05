ec85fe97e9c31453ebbed5add61612b63bb40b64
Arin Agarwal <arin.agarwal02@gmail.com>
2024-04-05 15:31:46 +0530

    Time: 0 ms (100.00%), Space: 7.7 MB (74.45%) - LeetHub

--- 1544-make-the-string-great/1544-make-the-string-great.cpp
+++ 1544-make-the-string-great/1544-make-the-string-great.cpp
@@ -1,30 +1,34 @@
 class Solution {
 public:
     string makeGood(string s) {
-        stack<char> st;
+        // stack<char> st;
+        // use ans as stack in this 
         string ans;
         
         for(auto &it: s){    
-            if(st.empty()){
-                st.push(it);
+            if(ans.empty()){
+                // st.push(it);
+                ans.push_back(it);
                 continue;
             }
             
-            int a = abs(it-st.top());
+            int a = abs(it-ans.back());
             
             if(a == 32){
-                st.pop();
+                // st.pop();
+                ans.pop_back();
             }
             else{
-                st.push(it);
+                // st.push(it);
+                ans.push_back(it);
             }
         }
         
-        while(!st.empty()){
-            // ans += st.top();
-            ans = st.top()+ans;
-            st.pop();
-        }
+        // while(!st.empty()){
+        //     ans += st.top();
+        //     // ans = st.top()+ans;
+        //     st.pop();
+        // }
         
         // reverse(ans.begin(),ans.end());
         return ans;
