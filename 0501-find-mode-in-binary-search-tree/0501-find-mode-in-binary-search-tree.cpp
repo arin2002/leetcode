a0fa68925b3de199fcc894f4f4f2f4556632f090
Arin Agarwal <arin.agarwal02@gmail.com>
2024-04-17 13:54:06 +0530

    Time: 18 ms (34.75%), Space: 22.4 MB (76.75%) - LeetHub

--- 0501-find-mode-in-binary-search-tree/0501-find-mode-in-binary-search-tree.cpp
+++ 0501-find-mode-in-binary-search-tree/0501-find-mode-in-binary-search-tree.cpp
@@ -1,34 +1,38 @@
 class Solution {
 public:
-    unordered_map<int,int>mp;
-
-    void isValid(TreeNode* root) {
-
-        if(root==NULL) return;
-
-        isValid(root->left);
-        mp[root->val]++;
-        isValid(root->right);
-
-    }
-
     vector<int> findMode(TreeNode* root) {
-
-        isValid(root);
-
-        int maxi=0;
-        for(auto it:mp){             
-            if(it.second>maxi){
-                maxi=it.second;
-            }
-        }
+        vector<int> ans;
+        int maxcnt = 0, cnt = 0;
+        int prev = -1; // Initialize prev to a value that won't be encountered in the tree
         
-        vector<int>ans;
-        for(auto it:mp){
-            if(it.second==maxi){
-                ans.push_back(it.first);
+        function<void(TreeNode*)> solve = [&](TreeNode* root){
+            if(!root){
+                return;
             }
-        }
+            
+            solve(root->left);
+            int curr = root->val;
+            
+            if(curr == prev){
+                cnt++;
+            }
+            else{
+                cnt = 1; // Reset cnt for a new value
+            }
+            
+            if(cnt >= maxcnt){
+                if(cnt > maxcnt){
+                    ans.clear(); // Clear ans when we find a new maximum frequency
+                    maxcnt = cnt;
+                }
+                ans.push_back(curr);
+            }
+            
+            prev = curr; // Update prev for the next iteration
+            solve(root->right);
+        };
+        
+        solve(root);
         return ans;
     }
 };
