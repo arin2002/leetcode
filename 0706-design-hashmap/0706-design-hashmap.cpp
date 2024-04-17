b343c34f9ff3ee5dfbb0807fa115726ddfde3c03
Arin Agarwal <arin.agarwal02@gmail.com>
2024-04-17 20:35:33 +0530

    Time: 107 ms (48.18%), Space: 66 MB (53.38%) - LeetHub

--- 0706-design-hashmap/0706-design-hashmap.cpp
+++ 0706-design-hashmap/0706-design-hashmap.cpp
@@ -1,22 +1,62 @@
 class MyHashMap {
 public:
     
-    vector<int> ump;
+    // hash function key%size -> collisons (chaining must be done)
+    // load factor hota h uske through chain ka size decide karenge
     
+    // vector of linked list
+    vector<list<pair<int,int>>> hashmap;
+    int size = 1e4+1;
+
     MyHashMap() {
-        ump.resize(1000001,-1);
+        hashmap.resize(size);
     }
     
     void put(int key, int value) {
-        ump[key] = value;
+        // hash function
+        int bucket_no = key%size;
+        // chain where we will add
+        auto &chain = hashmap[bucket_no];
+        
+        for(auto &[a,b]: chain){
+            if(a == key){
+                b = value;
+                return;
+            }
+        }
+        
+        chain.emplace_back(key,value);
     }
     
     int get(int key) {
-        return ump[key];
+        // hash function
+        int bucket_no = key%size;
+        // chain where we will add
+        auto &chain = hashmap[bucket_no];
+        
+        for(auto &[a,b]: chain){
+            if(a == key){
+                return b;
+            }
+        }
+        
+        return -1;
     }
     
     void remove(int key) {
-        ump[key] = -1;
+        // hash function
+        int bucket_no = key%size;
+        // chain where we will add
+        auto &chain = hashmap[bucket_no];
+        
+        for(auto it = chain.begin(); it != chain.end(); it++){
+            auto k = it->first;
+            
+            if(k == key){
+                chain.erase(it);
+                return;
+            }
+        }
     }
 };
 
