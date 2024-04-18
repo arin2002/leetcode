2b6c89a31d354ac27204cc6b494c056c31955c7f
Arin Agarwal <arin.agarwal02@gmail.com>
2024-04-18 11:24:14 +0530

    Time: 76 ms (51.27%), Space: 105.3 MB (10.59%) - LeetHub

--- 0463-island-perimeter/0463-island-perimeter.cpp
+++ 0463-island-perimeter/0463-island-perimeter.cpp
@@ -4,6 +4,7 @@ public:
         int n = grid.size(), m = grid[0].size();
         vector<vector<int>> vis(n,vector<int>(m));
         vector<int> drs{-1,0,1,0,-1};
+        int ans = 0;
         
         function<bool(int,int)> check = [&](int i, int j){
             if(i<0 || i == n || j<0 || j == m || grid[i][j] == 0)
@@ -11,42 +12,23 @@ public:
 
             return false;
         };
-        
-        function<int(int,int)> solve = [&](int i, int j){
-            if(i<0 || i == n || j<0 || j == m || vis[i][j] || grid[i][j] == 0){
-                return 0;
-            }
-            
-            vis[i][j] = 1;
-            int ans = 0;
-            
-            for(int k = 0; k<4; k++){
-                int nx = i+drs[k];
-                int ny = j+drs[k+1];
-                
-                if(check(nx,ny)){
-                    ans += 1;
-                }
-            }
-            
-            for(int k = 0; k<4; k++){
-                int nx = i+drs[k];
-                int ny = j+drs[k+1];
-                
-                ans += solve(nx,ny);
-            }
-            
-            return ans;
-        };
-        
+       
         for(int i = 0; i<n; i++){
             for(int j = 0; j<m; j++){
-                if(grid[i][j]){
-                    return solve(i,j);
+                if(!grid[i][j])
+                    continue;
+                
+                for(int k = 0; k<4; k++){
+                    int nx = i + drs[k];
+                    int ny = j + drs[k+1];
+                    
+                    if(check(nx,ny)){
+                        ans++;
+                    }
                 }
             }
         }
         
-        return 0;
+        return ans;
     }
 };
