69ed5e8f47a00b5b1c28cc8c843d0bd614435cf5
Arin Agarwal <arin.agarwal02@gmail.com>
2024-04-19 18:59:04 +0530

    Time: 11 ms (47.13%), Space: 14.5 MB (43.24%) - LeetHub

--- 1436-destination-city/1436-destination-city.cpp
+++ 1436-destination-city/1436-destination-city.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     string destCity(vector<vector<string>>& paths) {
-        unordered_map<string,string> ump;
+        map<string,string> ump;
         
         for(auto &it: paths){
             ump[it[0]] = it[1];
