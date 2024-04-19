86d9b1233e3de094c562e5b98aa706d7f69805fe
Arin Agarwal <arin.agarwal02@gmail.com>
2024-04-19 11:06:06 +0530

    Time: 32 ms (39.58%), Space: 15.9 MB (62.14%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -1,28 +1,31 @@
 class Solution {
 public:
-    
-    void dfs(int i, int j, int n, int m, vector<vector<char>>& grid){
-        if(i<0 || i>=n || j<0 || j>=m || grid[i][j] == '0')
-            return;
-        
-        grid[i][j] = '0';
-        
-        dfs(i+1,j,n,m,grid);
-        dfs(i,j+1,n,m,grid);
-        dfs(i-1,j,n,m,grid);
-        dfs(i,j-1,n,m,grid);
-    }
-    
-    
     int numIslands(vector<vector<char>>& grid) {
-        int n =  grid.size(), m = grid[0].size(), ans = 0;
-
+        int n = grid.size(), m = grid[0].size();
+        vector<int> drs{-1,0,1,0,-1};
+        int ans = 0;
+        
+        function<void(int,int)> solve = [&](int i, int j){
+            if(i<0 || i == n || j<0 || j == m || grid[i][j] == '0')
+                return;
+            
+            grid[i][j] = '0';
+            
+            for(int k = 0; k<4; k++){
+                int nx = i+drs[k], ny = j+drs[k+1];
+                
+                solve(nx,ny);
+            }
+            
+        };
+        
         for(int i = 0; i<n; i++){
             for(int j = 0; j<m; j++){
-                if(grid[i][j] == '1'){
-                    dfs(i,j,n,m,grid);
-                    ans++;
-                }
+                if(grid[i][j] == '0')
+                    continue;
+                
+                ans++;
+                solve(i,j);
             }
         }
         
