5b891217a4f921dceb223c061d4d102b3e409112
Arin Agarwal <arin.agarwal02@gmail.com>
2024-04-20 20:49:52 +0530

    Time: 115 ms (53.82%), Space: 70.6 MB (63.27%) - LeetHub

--- 1992-find-all-groups-of-farmland/1992-find-all-groups-of-farmland.cpp
+++ 1992-find-all-groups-of-farmland/1992-find-all-groups-of-farmland.cpp
@@ -1,43 +1,44 @@
 class Solution {
 public:
     vector<vector<int>> findFarmland(vector<vector<int>>& land) {
-        vector<vector<bool>> visited(land.size(), vector<bool>(land[0].size(), false));
         vector<vector<int>> ans;
-        for (int row1 = 0; row1 < land.size(); row1++) {
-            for (int col1 = 0; col1 < land[0].size(); col1++) {
-                if (land[row1][col1] && !visited[row1][col1]) {
-                    int row2 = 0, col2 = 0;
-                    DFS(land, visited, row1, col1, row2, col2);
-                    ans.push_back({row1, col1, row2, col2});
+        int n = land.size(), m = land[0].size();
+        int r = 0, c = 0;
+        vector<int> drs{-1, 0, 1, 0, -1};
+
+        // Traverse the grid
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (land[i][j] == 0 || land[i][j] == 2) {
+                    continue;
                 }
+
+                r = 0, c = 0;
+                solve(i, j, n, m, land, r, c, drs, ans); // Call solve function
+                ans.push_back({i, j, r, c});
             }
         }
-        
+
         return ans;
     }
 
-private:
-    // The four directions in which traversal will be done.
-    int dirs[4][2] = {{-1, 0}, {0, -1}, {0, 1}, {1, 0}};
-    // Returns true if the coordinate is within the boundary of the matrix.
-    bool isWithinFarm(int x, int y, int N, int M) {
-        return x >= 0 && x < N && y >= 0 && y < M;
-    }
-    
-    void DFS(vector<vector<int>>& land, vector<vector<bool>>& visited, int x, int y, int& row2,
-        int& col2) {
-        visited[x][y] = true;
-        // Maximum x and y for the bottom right cell.
-        row2 = max(row2, x); col2 = max(col2, y);
-        
-        for (auto dir : dirs) {
-            // Neighbor cell coordinates.
-            int newX = x + dir[0], newY = y + dir[1];
-            // If the neighbor is within the matrix and is a farmland cell and is not visited yet.
-            if (isWithinFarm(newX, newY, land.size(), land[0].size()) && !visited[newX][newY]
-                    && land[newX][newY]) {
-                DFS(land, visited, newX, newY, row2, col2);
-            }
+    // Definition of solve function outside of findFarmland
+    void solve(int i, int j, int n, int m, vector<vector<int>>& land, int& r, int& c, const vector<int>& drs, vector<vector<int>>& ans) {
+        if (i < 0 || j < 0 || i == n || j == m)
+            return;
+
+        if (land[i][j] == 0 || land[i][j] == 2)
+            return;
+
+        land[i][j] = 2;
+        r = max(r, i);
+        c = max(c, j);
+
+        for (int k = 0; k < 4; k++) {
+            int nx = i + drs[k];
+            int ny = j + drs[k + 1];
+
+            solve(nx, ny, n, m, land, r, c, drs, ans); // Call solve recursively
         }
-    }    
+    }
 };
