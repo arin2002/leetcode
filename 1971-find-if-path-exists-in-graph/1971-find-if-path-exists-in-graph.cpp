88a02f705514d98f9736fbcea0050fbe9fa13eb9
Arin Agarwal <arin.agarwal02@gmail.com>
2024-04-21 11:16:00 +0530

    Time: 467 ms (56.51%), Space: 176.5 MB (63.54%) - LeetHub

--- 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.cpp
+++ 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.cpp
@@ -2,31 +2,35 @@ class Solution {
 public:
     bool validPath(int n, vector<vector<int>>& edges, int source, int destination) {
         vector<int> adj[n], vis(n);
-        
-        for(auto &it: edges){
+
+        // Populate adjacency list
+        for (auto &it : edges) {
             adj[it[0]].push_back(it[1]);
             adj[it[1]].push_back(it[0]);
         }
-        
-        function<bool(int)> dfs = [&](int node){
-            if(node == destination){
+
+        // Depth-first search function
+        return dfs(source, destination, adj, vis);
+    }
+
+    // Depth-first search function definition
+    bool dfs(int node, int destination, vector<int> adj[], vector<int>& vis) {
+        if (node == destination) {
+            return true;
+        }
+
+        vis[node] = 1;
+        for (auto &it : adj[node]) {
+            if (vis[it]) {
+                continue;
+            }
+
+            vis[it] = 1;
+            if (dfs(it, destination, adj, vis)) {
                 return true;
             }
-            
-            vis[node] = 1;
-            for(auto &it: adj[node]){
-                if(vis[it])
-                    continue;
-                
-                vis[it] = 1;
-                if(dfs(it)){    
-                    return true;
-                }
-            }
-            
-            return false;
-        };
-        
-        return dfs(source);
+        }
+
+        return false;
     }
 };
