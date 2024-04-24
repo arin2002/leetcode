3ef2b7c2c10c851d56c0f191a22e992eac19e741
Arin Agarwal <arin.agarwal02@gmail.com>
2024-04-24 10:52:43 +0530

    Time: 0 ms (100.00%), Space: 7 MB (97.26%) - LeetHub

--- 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.cpp
+++ 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.cpp
@@ -1,25 +1,18 @@
 class Solution {
 public:
     int tribonacci(int n) {
-        int t0 = 0, t1 = 1, t2 = 1;
+        int a = 0, b = 1, c = 1, ans = 1;
         
-        if(n == 0)
-            return 0;
-        else if(n == 1)
-            return 1;
-        else if(n == 2)
-            return 1;
+        if(n <= 1)
+            return n;
         
-        int sum = 0, t3;
         for(int i = 2; i<n; i++){
-            // cout<<t1<<" "<<t2<<" "<<t3<<endl;
-            t3 = t0+t1+t2;
-            
-            t0 = t1;
-            t1 = t2;
-            t2 = t3;
+            ans = a+b+c;
+            a = b;
+            b = c;
+            c = ans;
         }
         
-        return t3;
+        return ans;
     }
 };
