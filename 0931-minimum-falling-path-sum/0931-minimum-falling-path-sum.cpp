e3b7515cd61e025560b375dde6d71947b97a50a0
Arin Agarwal <arin.agarwal02@gmail.com>
2024-04-26 12:19:31 +0530

    Time: 20 ms (5.89%), Space: 14.7 MB (5.03%) - LeetHub

--- 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.cpp
+++ 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.cpp
@@ -1,26 +1,34 @@
 class Solution {
 public:
     int minFallingPathSum(vector<vector<int>>& matrix) {
-       int n = matrix.size();
-         
-       int mini = INT_MAX;
-       vector<vector<int>> dp(n, vector<int>(n,INT_MAX));
-       for (int i = 0; i < n; i++) {
-           mini = min(mini, solve(0, i, matrix, n, dp));
-       }
-       return mini;
-    }
-
-    int solve(int row, int col, vector<vector<int>>& matrix, int n, vector<vector<int>>& dp) {
-        if (row == n) return 0; // Change condition to row == n
-        if (col < 0 || col >= n) return INT_MAX; // Change col > n-1 to col >= n
-        if (dp[row][col] != INT_MAX) return dp[row][col]; 
-      
-        dp[row][col] = matrix[row][col] + min(
-            solve(row + 1, col, matrix, n, dp),
-            min(solve(row + 1, col + 1, matrix, n, dp), solve(row + 1, col - 1, matrix, n, dp))
-        );
-
-        return dp[row][col];
+        vector<vector<int>> drs = {{1,-1},{1,0},{1,1}};
+        int n = matrix.size(), m = matrix[0].size();
+        vector<vector<int>> dp(n,vector<int>(m,INT_MAX)); 
+        
+        function<int(int,int)> solve = [&](int i, int j){
+            if(i == n || j == m || i < 0 || j < 0)
+                return (int)1e6;
+            
+            if(i == n-1)
+                return matrix[i][j];
+            
+            if(dp[i][j] != INT_MAX)
+                return dp[i][j];
+            
+            int ans = INT_MAX;
+            for(auto &it: drs){
+                int nx = i + it[0], ny = j + it[1];
+                ans = min(ans, matrix[i][j] + solve(nx, ny));
+            }
+            
+            return  dp[i][j] = ans;
+        };
+        
+        int ans = INT_MAX;
+        for(int j = 0; j < m; j++){
+            ans = min(ans, solve(0, j));
+        }
+        
+        return ans;
     }
 };
