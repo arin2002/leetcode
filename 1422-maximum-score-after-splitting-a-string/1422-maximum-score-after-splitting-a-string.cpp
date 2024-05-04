cad38bdf1e615978962ff1baba5c5cebc24d6c98
Arin Agarwal <arin.agarwal02@gmail.com>
2024-05-04 10:44:41 +0530

    Time: 0 ms (100.00%), Space: 7.6 MB (26.04%) - LeetHub

--- 1422-maximum-score-after-splitting-a-string/1422-maximum-score-after-splitting-a-string.cpp
+++ 1422-maximum-score-after-splitting-a-string/1422-maximum-score-after-splitting-a-string.cpp
@@ -1,21 +1,29 @@
 class Solution {
 public:
     int maxScore(string s) {
-        int n = s.size();
-        int ones = count(s.begin(),s.end(),'1');
-        int zero = 0, one = 0, ans = 0;
+        int n = s.size(), ans  = -1, zeroL = 0, oneL = 0;
+        // ans = zeroL + oneR
+        // ans = zeroL + (total_1-oneL)
+        // ans = (zeroL-oneL) + total_1
+        // total_1 is constant hence we need to find
+        // max value of (zeroL-oneL) which depends on left only
         
         for(int i = 0; i<n-1; i++){
-            if(s[i] == '0')
-                zero++;
-            else
-                one++;
+            if(s[i] == '1'){
+                oneL++;
+            }
+            else{
+                zeroL++;
+            }
             
-            int net_one = ones-one;
-            
-            ans = max(ans,zero+net_one);
+            ans = max(ans,zeroL-oneL);
         }
         
-        return ans;
+        if (s[n - 1] == '1') {
+            oneL++;
+        }
+        
+        cout<<ans<<" "<<oneL;
+        return ans + oneL;
     }
 };
