f9242f6c6a7abb8d1a31589576c43adfabd539a4
Arin Agarwal <arin.agarwal02@gmail.com>
2024-05-08 15:17:54 +0530

    Time: 4 ms (67.04%), Space: 13.3 MB (63.51%) - LeetHub

--- 0086-partition-list/0086-partition-list.cpp
+++ 0086-partition-list/0086-partition-list.cpp
@@ -1,26 +1,61 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
 class Solution {
 public:
     ListNode* partition(ListNode* head, int x) {
-        if(head==NULL||head->next==NULL)return head;
-
-
-        ListNode*node=new ListNode(0);//Placing all lower in list
-        ListNode*high=new ListNode(0);//placing all higher in list
-        ListNode*mark1=node;
-        ListNode*mark2=high;
-
-        while(head!=NULL){
-            if(head->val<x){
-                node->next=new ListNode(head->val);
-                node=node->next;
+        ListNode *p = NULL, *q = NULL, *r = head;
+        ListNode *head1 = NULL, *head2 = NULL;
+        
+        if(head == NULL)
+            return head;
+        
+        while(r){
+            int val = r->val;
+            
+            if(val < x){
+                if(p == NULL){
+                    p = r;
+                    head1 = p;
+                }
+                else{
+                    p->next = r;
+                    p = r;
+                }
+        
             }
             else{
-                high->next=new ListNode(head->val);
-                high=high->next;
+                if(q == NULL){
+                    q = r;
+                    head2 = q;
+                }
+                else{
+                    q->next = r;
+                    q = r;
+                }
             }
-            head=head->next;
+            r = r->next;
         }
-        node->next=mark2->next;//merging two list lower than higher.
-        return mark1->next;
+        
+        if(head1 == NULL){
+            q->next = NULL;
+            return head2;
+        }
+        
+        if(head2 == NULL){
+            p->next = NULL;
+            return head1;
+        }
+        
+        p->next = head2;
+        q->next = NULL;    
+        return head1;
     }
 };
