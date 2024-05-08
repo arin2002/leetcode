05b98b08905571b2cea5dc453a1d36ace7b8914e
Arin Agarwal <arin.agarwal02@gmail.com>
2024-05-08 20:27:25 +0530

    Time: 7 ms (83.10%), Space: 18.2 MB (99.40%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.cpp
+++ 0229-majority-element-ii/0229-majority-element-ii.cpp
@@ -1,22 +1,56 @@
 class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
-        vector<int> ans ; 
-        map<int, int> freq; 
-         int n = nums.size(); 
-        for(int i =0; i < nums.size(); i++)
-        {
-            if(freq[nums[i]]>=n/3)
-            {
-                ans.push_back(nums[i]);
-                freq[nums[i]]= -1 ; 
-                continue; 
+        // bayers moyers voting algo
+        // n/k = (k-1) at max majorirty elemts
+        // proof by example see
+        // take 2 variables
+        // it based on fact that they cancel each other
+        // hence we will get n/k 
+        // in this verification must be done
+        
+        vector<int> ans;
+        int count1 = 0, count2 = 0, candidate1 = INT_MAX, candidate2 = INT_MAX;
+        int n = nums.size();
+        
+        for(auto &it: nums){
+            if(it == candidate1)
+                count1++;
+            
+            else if(it == candidate2)
+                count2++;
+            
+            else if(count1 == 0){
+                candidate1 = it;
+                count1 = 1;
             }
-            if(freq[nums[i]]== -1 )
-            continue; 
-           
-           freq[nums[i]]++;
+            else if(count2 == 0){
+                candidate2 = it;
+                count2 = 1;
+            }
+            else{
+                count1--;
+                count2--;
+            }
         }
-        return ans ;
+        
+        count1 = 0; count2 = 0;
+        for(auto &it: nums){
+            if(it == candidate1)
+                count1++;
+            
+            if(it == candidate2)
+                count2++;
+        }
+        
+        cout<<candidate2;
+        if(count1 > n/3)
+            ans.push_back(candidate1);
+        
+        if(count2 > n/3)
+            ans.push_back(candidate2);
+        
+        
+        return ans;
     }
 };
