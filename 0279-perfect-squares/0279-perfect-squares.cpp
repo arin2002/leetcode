891b841fd6e5374e21dd2c649783a20681cdd7a8
Arin Agarwal <arin.agarwal02@gmail.com>
2024-05-09 15:12:50 +0530

    Time: 479 ms (7.85%), Space: 161.7 MB (5.09%) - LeetHub

--- 0279-perfect-squares/0279-perfect-squares.cpp
+++ 0279-perfect-squares/0279-perfect-squares.cpp
@@ -1,35 +1,35 @@
 class Solution {
-    int targetSum(vector<int> &perfectSquares, int target, vector<int> &dp){
-        if(target == 0)
-            return 0;
-        
-        if(target < 0)
-            return INT_MAX;
-        
-        if(dp[target] != INT_MAX)
-            return dp[target];
-        int minPerfectSquares = INT_MAX;
-        for(int i = 0; i < perfectSquares.size(); i++){
-            int ans = targetSum(perfectSquares, target-perfectSquares[i],dp);
-            if(ans != INT_MAX)
-                minPerfectSquares = min(minPerfectSquares, ans+1);
-        }
-        return dp[target] = minPerfectSquares;
-    }
 public:
     int numSquares(int n) {
-        vector<int> perfectSquares;
+        vector<int> nums;
         
-        //perfectSquares that can be added to get the sum = n;
-        int i = 1;
-        while(i*i <= n){
-            cout<<i*i<<endl;
-            perfectSquares.push_back(i*i);
-            i++;
+        for(int i = 1; i*i<=n; i++){
+            nums.push_back(i*i);
         }
-        vector<int> dp(n+1, INT_MAX);
-        //now the question has been reduced to target sum question
-        int minPerfectSquare = targetSum(perfectSquares, n, dp);
-        return minPerfectSquare;
+                
+        int m = nums.size();
+        vector<vector<int>> dp(m+1,vector<int>(n+1,-1));
+        
+        function<int(int,int)> solve = [&](int ind, int n){
+            if(n < 0)
+                return (int)1e6;
+            
+            if(n == 0)
+                return 0;
+            
+            if(ind < 0)
+                return (int)1e6;
+            
+            if(dp[ind][n] != -1)
+                return dp[ind][n];
+            
+            // int cnt = n%nums[ind]
+            int take = 1+solve(ind,n-nums[ind]);
+            int not_take = 0+solve(ind-1,n);
+            
+            return dp[ind][n] = min(take,not_take);
+        };
+        
+        return solve(m-1,n);
     }
 };
