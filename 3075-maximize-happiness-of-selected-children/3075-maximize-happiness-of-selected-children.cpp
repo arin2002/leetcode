de6b85242925480ee39b310fa6c85e4e29b57457
Arin Agarwal <arin.agarwal02@gmail.com>
2024-05-09 10:06:36 +0530

    Time: 403 ms (5.07%), Space: 112.7 MB (11.29%) - LeetHub

--- 3075-maximize-happiness-of-selected-children/3075-maximize-happiness-of-selected-children.cpp
+++ 3075-maximize-happiness-of-selected-children/3075-maximize-happiness-of-selected-children.cpp
@@ -2,17 +2,26 @@ class Solution {
 public:
     long long maximumHappinessSum(vector<int>& happiness, int k) {
         long long ans = 0, n = happiness.size();
-        int cnt = 0;
+        int cnt = k-1;
         // priority_queue<int> pq(happiness.begin(),happiness.end());
-        sort(happiness.begin(),happiness.end(), greater<int>());
+        // sort(happiness.begin(),happiness.end(), greater<int>());
         
-        for(int i = 0; i<n; i++){
-            if(k == 0 || happiness[i]-cnt <= 0)
-                break;
+        // we need k highest
+        priority_queue<int,vector<int>,greater<int>> pq;
+        
+        for(auto &it: happiness){
+            pq.push(it);
             
-            ans += (long long)(happiness[i]-cnt);
-            k--;
-            cnt++;
+            if(pq.size()>k)
+                pq.pop();
+        }
+        
+        while(!pq.empty()){
+            
+            cout<<pq.top()-cnt<<" ";
+            ans += (long long)max(pq.top()-cnt,0);
+            pq.pop();
+            cnt--;
         }
         
         return ans;
