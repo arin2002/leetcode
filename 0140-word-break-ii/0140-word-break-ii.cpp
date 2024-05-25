632736bed970904e8e8a060c45cbe5be195f834f
Arin Agarwal <arin.agarwal02@gmail.com>
2024-05-25 11:37:55 +0530

    Time: 4 ms (32.03%), Space: 8.8 MB (77.56%) - LeetHub

--- 0140-word-break-ii/0140-word-break-ii.cpp
+++ 0140-word-break-ii/0140-word-break-ii.cpp
@@ -1,18 +1,14 @@
 class Solution {
 public:
     vector<string> wordBreak(string s, vector<string>& wordDict) {
-        vector<string> ans, t;
+        vector<string> ans;
         unordered_set<string> st(wordDict.begin(),wordDict.end());
         int n = s.size();
         
-        function<void(int)> solve = [&](int ind){
+        function<void(int,string)> solve = [&](int ind, string t){
             if(ind == n){
-                string temp;
-                for(auto &it: t){
-                    temp += it+" ";
-                }
-                temp.pop_back();
-                ans.push_back(temp);
+                t.pop_back();
+                ans.push_back(t);
                 return;
             }
             
@@ -21,15 +17,13 @@ public:
                 temp += s[i];
                 auto it = st.find(temp);
                 
-                t.push_back(temp);
                 if(it != st.end()){
-                    solve(i+1);
+                    solve(i+1,t+temp+" ");
                 }
-                t.pop_back();
             }
         };
         
-        solve(0);
+        solve(0,"");
         return ans;
     }
 };
