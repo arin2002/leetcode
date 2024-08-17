f9904f927d407e7f9171fbb6e224b343862f8697
Arin Agarwal <arin.agarwal02@gmail.com>
2024-08-17 20:34:31 +0530

    Time: 221 ms (29.97%), Space: 129.1 MB (32.97%) - LeetHub

---
+++ 1937-maximum-number-of-points-with-cost/1937-maximum-number-of-points-with-cost.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    long long maxPoints(vector<vector<int>>& points) {
+        int m = points.size(), n = points[0].size();
+        vector<long long> dp(n, 0);
+
+        for (int j = 0; j < n; ++j) {
+            dp[j] = points[0][j];
+        }
+
+        for (int i = 1; i < m; ++i) {
+            vector<long long> left(n, 0), right(n, 0), new_dp(n, 0);
+
+            left[0] = dp[0];
+            for (int j = 1; j < n; ++j) {
+                left[j] = max(left[j - 1], dp[j] + j);
+            }
+
+            right[n - 1] = dp[n - 1] - (n - 1);
+            for (int j = n - 2; j >= 0; --j) {
+                right[j] = max(right[j + 1], dp[j] - j);
+            }
+
+            for (int j = 0; j < n; ++j) {
+                new_dp[j] = max(left[j] - j, right[j] + j) + points[i][j];
+            }
+
+            dp = new_dp;
+        }
+
+        return *max_element(dp.begin(), dp.end());
+    }
+};
