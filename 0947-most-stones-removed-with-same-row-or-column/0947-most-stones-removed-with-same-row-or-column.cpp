4c6300d3454be5cea92f1d3f3bac3032a186a2ba
Arin Agarwal <arin.agarwal02@gmail.com>
2024-08-29 23:10:15 +0530

    Time: 65 ms (32.38%), Space: 18.3 MB (79.11%) - LeetHub

--- 0947-most-stones-removed-with-same-row-or-column/0947-most-stones-removed-with-same-row-or-column.cpp
+++ 0947-most-stones-removed-with-same-row-or-column/0947-most-stones-removed-with-same-row-or-column.cpp
@@ -1,22 +1,54 @@
 class Solution {
 public:
+int n;
+vector<int>parent;
+vector<int>rank;
+int find(vector<int>&parent,int x){
+    if(x==parent[x]) return x;
+    return parent[x]=find(parent,parent[x]);
+}
+void Union(int x,int y)
+{
+    int px=find(parent,x);
+    int py=find(parent,y);
+    if(px==py) return;
+    if(rank[px]>rank[py])
+    {
+        parent[py]=px;
+    }
+    else if(rank[px]<rank[py])
+    {
+        parent[px]=py;
+    }
+    else{
+        parent[py]=px;
+        rank[px]++;
+    }
+}
     int removeStones(vector<vector<int>>& stones) {
-        for (int i = 0; i < stones.size(); ++i)
-            uni(stones[i][0], ~stones[i][1]);
-        return stones.size() - islands;
-    }
-
-    unordered_map<int, int> f;
-    int islands = 0;
-
-    int find(int x) {
-        if (!f.count(x)) f[x] = x, islands++;
-        if (x != f[x]) f[x] = find(f[x]);
-        return f[x];
-    }
-
-    void uni(int x, int y) {
-        x = find(x), y = find(y);
-        if (x != y) f[x] = y, islands--;
+        n=stones.size();
+        parent.resize(n);
+        rank.resize(n);
+        for(int i=0;i<n;i++)
+        {
+            parent[i]=i;
+            rank[i]=0;
+        }
+        for(int i=0;i<n;i++)
+        {
+            for(int j=i+1;j<n;j++)
+            {
+                if(stones[i][0]==stones[j][0] || stones[i][1]==stones[j][1])
+                {
+                    Union(i,j);
+                }
+            }
+        }
+        int groups=0;
+        for(int i=0;i<n;i++)
+        {
+            if(i==parent[i]) groups++;
+        }
+        return n-groups;
     }
 };
