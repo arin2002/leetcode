fc7f6d9f3c0c64d2f6a0dc10e6774825439537ef
Arin Agarwal <arin.agarwal02@gmail.com>
2024-10-19 18:02:19 +0530

    Time: 0 ms (100.00%), Space: 7.4 MB (87.54%) - LeetHub

--- 1545-find-kth-bit-in-nth-binary-string/1545-find-kth-bit-in-nth-binary-string.cpp
+++ 1545-find-kth-bit-in-nth-binary-string/1545-find-kth-bit-in-nth-binary-string.cpp
@@ -1,27 +1,24 @@
 class Solution {
 public:
-    
-    string f(string &ans){
-        string invert = "";
-        
-        for(auto it : ans){
-            if(it == '0')
-                invert += '1';
-            else
-                invert += '0';
-        }
-        
-        reverse(invert.begin(),invert.end());
-        return invert;
-    }
-    
     char findKthBit(int n, int k) {
-        string ans = "0";
-        
-        while(--n){
-            ans = ans + "1" + f(ans);
+        // Find the position of the rightmost set bit in k
+        // This helps determine which "section" of the string we're in
+        int positionInSection = k & -k;
+
+        // Determine if k is in the inverted part of the string
+        // This checks if the bit to the left of the rightmost set bit is 1
+        bool isInInvertedPart = ((k / positionInSection) >> 1 & 1) == 1;
+
+        // Determine if the original bit (before any inversion) would be 1
+        // This is true if k is even (i.e., its least significant bit is 0)
+        bool originalBitIsOne = (k & 1) == 0;
+
+        if (isInInvertedPart) {
+            // If we're in the inverted part, we need to flip the bit
+            return originalBitIsOne ? '0' : '1';
+        } else {
+            // If we're not in the inverted part, return the original bit
+            return originalBitIsOne ? '1' : '0';
         }
-        
-        return ans[k-1];
     }
 };
